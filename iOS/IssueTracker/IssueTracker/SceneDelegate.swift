3697fb4b0ac48394e5605e13e1a325ac1b22a7aa
Jason <ggj00125@gmail.com>
2022-06-13 18:46:41 +0900

    feat: 프로젝트 생성

---
+++ iOS/IssueTracker/IssueTracker/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  IssueTracker
+//
+//  Created by Jason on 2022/06/13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let rootVC = ViewController()
+        self.window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+    }
+
+}
+
