7e12ba00a73b96f8170871a9a330605f34f6bc9b
P_SeoB <akdlsjtm21@naver.com>
2022-06-13 12:10:45 +0900

    init commit

---
+++ IssueTracker15_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IssueTracker15_1
+//
+//  Created by 박진섭 on 2022/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
