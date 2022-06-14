603ae1748112c52b487e87cc09e6d28fa604b509
Minseok-Choi <cms05041@gmail.com>
2022-06-14 14:41:22 +0900

    feat: Issue, Member 엔티티 타입, 생성자, 연관관계 매핑

--- BE/src/main/java/com/ron2ader/issueTracker/domain/issue/Issue.java
+++ BE/src/main/java/com/ron2ader/issueTracker/domain/issue/Issue.java
@@ -1,11 +1,28 @@
 package com.ron2ader.issueTracker.domain.issue;
 
 import com.ron2ader.issueTracker.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class Issue {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member issuer;
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer issueNumber;
 
     private String title;
@@ -15,5 +32,10 @@ public class Issue {
     private String attachmentUrl;
 
     private Boolean isOpen;
+
+    public static Issue of(Member member, String title, String contents, String attachmentUrl) {
+        return new Issue(null, member, null,
+                title, contents, attachmentUrl, true);
+    }
 
 }
--- BE/src/main/java/com/ron2ader/issueTracker/domain/member/Member.java
+++ BE/src/main/java/com/ron2ader/issueTracker/domain/member/Member.java
@@ -1,9 +1,29 @@
 package com.ron2ader.issueTracker.domain.member;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String userId;
 
     private String avatarUrl;
+
+    public static Member of(String userId, String avatarUrl) {
+        return new Member(null, userId, avatarUrl);
+    }
+
 
 }
