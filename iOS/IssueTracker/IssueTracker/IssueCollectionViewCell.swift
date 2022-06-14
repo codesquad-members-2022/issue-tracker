b2ce4433f7b7d514e0d354ecad1a23934753e829
Jason <ggj00125@gmail.com>
2022-06-14 14:27:29 +0900

    feat: IssueCollectionViewCell 생성

---
+++ iOS/IssueTracker/IssueTracker/IssueCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IssueCollectionViewCell.swift
+//  IssueTracker
+//
+//  Created by 이건행 on 2022/06/14.
+//
+
+import UIKit
+
+final class IssueCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError()
+    }
+    
+    
+}
