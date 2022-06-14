46fd320cbe6e25a6cea9edf0678ba6519a3c261b
Nick <decay95@naver.com>
2022-06-14 20:31:26 +0900

    [feat]: addIssue Page 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '@styles/reset.css';
 import { Header } from '@components/header';
+import { AddIssue } from './pages/addIssue';
 
 const App = () => {
   return (
     <>
       <Header />
+      <AddIssue />
     </>
   );
 };
