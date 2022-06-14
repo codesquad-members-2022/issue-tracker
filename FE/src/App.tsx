82030799d0d19c2870c200b8603ce94a380dc674
Ubeom Kim <kimub1204@gmail.com>
2022-06-14 14:18:24 +0900

    init: OAuth 키 셋팅

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      const data = await axios.get(
+        'https://github.com/login/oauth/authorize?client_id=6996df6e85cf747c9098&scope=repo:status read:repo_hook user:email&redirect_uri=http://localhost:3000',
+      );
+      console.log(data);
+    })();
+  }, []);
+
   return <div />;
 }
 
