dcaa87b2c7628d6d8718ce8c3813c037e78d1710
Jason <ggj00125@gmail.com>
2022-06-14 14:24:58 +0900

    feat: IssueCollectionView 생성

--- iOS/IssueTracker/IssueTracker/IssueCollectionView.swift
+++ iOS/IssueTracker/IssueTracker/IssueCollectionView.swift
@@ -5,4 +5,20 @@
 //  Created by 이건행 on 2022/06/14.
 //
 
-import Foundation
+import UIKit
+
+final class IssueCollectionView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError()
+    }
+    
+    
+}
