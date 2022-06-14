f471036283f63b62064af07bc65ed0d496f127d6
Linus <zerolecture0922@gmail.com>
2022-06-14 16:48:00 +0900

    design : 필터 UI 구성 중

---
+++ app/src/main/java/com/example/issu_tracker/filter/CustomToolbar.kt
@@ -0,0 +1,9 @@
+package com.example.issu_tracker.filter
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.Toolbar
+
+class CustomToolbar(context: Context, attrs: AttributeSet) : Toolbar(context, attrs) {
+    
+}
