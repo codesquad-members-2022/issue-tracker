7ed48e251adedd11d0b178319b0db0b9ac3a2307
WooJin Park(Ader) <ak2j38@gmail.com>
2022-06-14 10:56:08 +0900

    ⬆️ 프로젝트 의존성 수정 및 추가
    Co-authored-by: CMSSKKK <cms05041@gmail.com>

---
+++ BE/src/test/java/com/ron2ader/issueTracker/IssueTrackerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.ron2ader.issueTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
