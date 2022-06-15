eb5799953a678318c3f24114e046a10c0ad5df09
ikjo93 <auddlr100@naver.com>
2022-06-16 01:35:46 +0900

    fix : ddl, dml 파일 도메인 리팩토링 결과 반영

--- be/src/main/resources/schema.sql
+++ be/src/main/resources/schema.sql
@@ -1,93 +1,93 @@
-DROP TABLE IF EXISTS issue_milestone;
 DROP TABLE IF EXISTS issue_label;
-DROP TABLE IF EXISTS milestone;
 DROP TABLE IF EXISTS label;
 DROP TABLE IF EXISTS reply;
-DROP TABLE IF EXISTS issue_assignee;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS assignee;
 DROP TABLE IF EXISTS issue;
-DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS milestone;
+DROP TABLE IF EXISTS writer;
 
-CREATE TABLE member
+CREATE TABLE writer
 (
-    member_id   INT NOT NULL AUTO_INCREMENT,
+    writer_id   BIGINT NOT NULL AUTO_INCREMENT,
     type        VARCHAR(32),
-    email       VARCHAR(255),
+    identity    VARCHAR(255),
     password    VARCHAR(255),
-    name        VARCHAR(32),
+    email       VARCHAR(255),
+    nickname    VARCHAR(32),
     profile_url VARCHAR(255),
-    PRIMARY KEY (member_id)
-);
-
-CREATE TABLE issue
-(
-    issue_id         INT NOT NULL AUTO_INCREMENT,
-    member_id        INT NOT NULL,
-    issue_number     INT,
-    subject          VARCHAR(255),
-    description      VARCHAR(255),
-    writer           VARCHAR(255),
-    image_url        VARCHAR(255),
-    status           VARCHAR(32),
-    created_datetime TIMESTAMP,
-    PRIMARY KEY (issue_id),
-    FOREIGN KEY (member_id) REFERENCES member (member_id)
-);
-
-CREATE TABLE issue_assignee
-(
-    issue_assignee_id INT NOT NULL AUTO_INCREMENT,
-    issue_id          INT NOT NULL,
-    member_id         INT NOT NULL,
-    PRIMARY KEY (issue_assignee_id),
-    FOREIGN KEY (issue_id) REFERENCES issue (issue_id),
-    FOREIGN KEY (member_id) REFERENCES member (member_id)
-);
-
-CREATE TABLE reply
-(
-    reply_id         INT NOT NULL AUTO_INCREMENT,
-    issue_id         INT NOT NULL,
-    member_id        INT NOT NULL,
-    content          VARCHAR(255),
-    created_datetime TIMESTAMP,
-    PRIMARY KEY (reply_id),
-    FOREIGN KEY (issue_id) REFERENCES issue (issue_id),
-    FOREIGN KEY (member_id) REFERENCES member (member_id)
-);
-
-CREATE TABLE label
-(
-    label_id    INT NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(64),
-    description VARCHAR(255),
-    PRIMARY KEY (label_id)
+    PRIMARY KEY (writer_id)
 );
 
 CREATE TABLE milestone
 (
-    milestone_id INT NOT NULL AUTO_INCREMENT,
+    milestone_id BIGINT NOT NULL AUTO_INCREMENT,
     subject      VARCHAR(64),
     description  VARCHAR(255),
-    end_time     TIMESTAMP,
+    end_date     TIMESTAMP,
     PRIMARY KEY (milestone_id)
+);
+
+CREATE TABLE issue
+(
+    issue_id         BIGINT NOT NULL AUTO_INCREMENT,
+    writer_id        BIGINT NOT NULL,
+    milestone_id     BIGINT,
+    subject          VARCHAR(255),
+    description      VARCHAR(255),
+    status           VARCHAR(32),
+    created_datetime TIMESTAMP,
+    updated_datetime TIMESTAMP,
+    PRIMARY KEY (issue_id),
+    FOREIGN KEY (writer_id) REFERENCES writer (writer_id),
+    FOREIGN KEY (milestone_id) REFERENCES milestone (milestone_id)
+);
+
+CREATE TABLE image (
+    image_id BIGINT NOT NULL AUTO_INCREMENT,
+    issue_id BIGINT NOT NULL,
+    image_url VARCHAR (255),
+    PRIMARY KEY (image_id),
+    FOREIGN KEY (issue_id) REFERENCES issue (issue_id)
+);
+
+CREATE TABLE assignee
+(
+    assignee_id       BIGINT NOT NULL AUTO_INCREMENT,
+    issue_id          BIGINT NOT NULL,
+    writer_id         BIGINT NOT NULL,
+    PRIMARY KEY (assignee_id),
+    FOREIGN KEY (issue_id) REFERENCES issue (issue_id),
+    FOREIGN KEY (writer_id) REFERENCES writer (writer_id)
+);
+
+CREATE TABLE reply
+(
+    reply_id         BIGINT NOT NULL AUTO_INCREMENT,
+    issue_id         BIGINT NOT NULL,
+    writer_id        BIGINT NOT NULL,
+    content          VARCHAR(255),
+    created_datetime TIMESTAMP,
+    PRIMARY KEY (reply_id),
+    FOREIGN KEY (issue_id) REFERENCES issue (issue_id),
+    FOREIGN KEY (writer_id) REFERENCES writer (writer_id)
+);
+
+CREATE TABLE label
+(
+    label_id    BIGINT NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(64),
+    description VARCHAR(255),
+    color       VARCHAR(64),
+    PRIMARY KEY (label_id)
 );
 
 CREATE TABLE issue_label
 (
-    issue_label_id INT NOT NULL AUTO_INCREMENT,
-    issue_id       INT NOT NULL,
-    label_id       INT NOT NULL,
+    issue_label_id BIGINT NOT NULL AUTO_INCREMENT,
+    issue_id       BIGINT NOT NULL,
+    label_id       BIGINT NOT NULL,
     PRIMARY KEY (issue_label_id),
     FOREIGN KEY (issue_id) REFERENCES issue (issue_id),
     FOREIGN KEY (label_id) REFERENCES label (label_id)
-);
-
-CREATE TABLE issue_milestone
-(
-    issue_milestone_id INT NOT NULL AUTO_INCREMENT,
-    issue_id           INT NOT NULL,
-    milestone_id       INT NOT NULL,
-    PRIMARY KEY (issue_milestone_id),
-    FOREIGN KEY (issue_id) REFERENCES issue (issue_id),
-    FOREIGN KEY (milestone_id) REFERENCES milestone (milestone_id)
 );
