521124beb74afbc3535ce7845aa54f8ca45ed6d1
Hemudi <ksum1205@naver.com>
2022-06-16 02:13:40 +0900

    Feat: Button Type 별 Style 속성 정의
    jindonyy/issue-tracker/#6

--- FE/src/components/button/button.style.ts
+++ FE/src/components/button/button.style.ts
@@ -1,5 +1,49 @@
-import styled, { css } from 'styled-components';
+import styled, { css, FlattenInterpolation, ThemeProps } from 'styled-components';
 
+type ButtonStyleType =
+  | 'Large'
+  | 'Small-Standard'
+  | 'Medium-Standard'
+  | 'Small-Secondary'
+  | 'Medium-Text'
+  | 'Small-Text';
+
+const large = css`
+  width: 340px;
+  height: 64px;
+  color: ${({ theme }) => theme.PALETTE.WHITE};
+  background: ${({ theme }) => theme.COLOR.primary.initial};
+  font-size: ${({ theme }) => `${theme.FONT.SIZE.MEDIUM}`};
+  font-weight: ${({ theme }) => `${theme.FONT.WEIGHT.BOLD}`};
+  border-radius: 20px;
+`;
+
+const smallStandard = css`
+  width: 120px;
+  height: 40px;
+  color: ${({ theme }) => theme.PALETTE.WHITE};
+  background: ${({ theme }) => theme.COLOR.primary.initial};
+  font-size: ${({ theme }) => `${theme.FONT.SIZE.X_SMALL}`};
+  font-weight: ${({ theme }) => `${theme.FONT.WEIGHT.BOLD}`};
+  border-radius: 10px;
+  svg {
+    path {
+      stroke: ${({ theme }) => theme.PALETTE.WHITE};
+    }
+  }
+import styled, { css } from 'styled-components';
+`;
+
+const mediumText = css`
+  width: 87px;
+  height: 32px;
+  color: ${({ theme }) => theme.COLOR.label};
+  svg {
+    path {
+      stroke: ${({ theme }) => theme.COLOR.label};
+    }
+  }
+`;
 interface Styled_button {
   width: string;
 }
