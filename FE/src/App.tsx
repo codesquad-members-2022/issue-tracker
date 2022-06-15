4b2424c433cd337db9d7df990f26d4cb5bfba613
jindonyy <rsefa94@gmail.com>
2022-06-16 03:26:45 +0900

    Design: input text 컴포넌트 구현
    closed jindonyy/issue-tracker/#7

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,4 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/GlobalStyle';
+import * as CommonStyle from '@/styles/common';
+
 const App = () => {
-  return <div>Hello Hojin</div>;
+  return (
+    <ThemeProvider theme={CommonStyle}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
 export default App;
