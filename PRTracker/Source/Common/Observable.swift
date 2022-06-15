52421e1dc425ba32e60721ae54d250fb88d3f730
sanghee <anshee97@naver.com>
2022-06-15 16:50:20 +0900

    Chore: Observable 추가

---
+++ PRTracker/Source/Common/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  PRTracker
+//
+//  Created by 안상희 on 2022/06/15.
+//
+
+import Foundation
+
+final class Observable<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
