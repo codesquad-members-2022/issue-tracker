67cb7a92f8713407bee1c19d07f69aa85761dfe6
sanghee <anshee97@naver.com>
2022-06-15 16:32:39 +0900

    chore: ListingViewController 스토리보드, 뷰컨트롤러 추가(BumgeunSong/PR-tracker#3)

---
+++ PRTracker/Source/PR_Listing/View/ListingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ListingViewController.swift
+//  PRTracker
+//
+//  Created by 안상희 on 2022/06/15.
+//
+
+import UIKit
+
+class ListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
