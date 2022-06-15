e6b6c32e17b0836d68698d03a4d55d71954c5192
PhilSoGooood <peelhw@gmail.com>
2022-06-15 22:01:46 +0900

    refactor: User와 Assignee의 매핑 관계 수정
    - 양방향 매핑에서 단방향 매핑으로 수정

--- BE/src/main/java/team24/issuetracker/domain/User.java
+++ BE/src/main/java/team24/issuetracker/domain/User.java
@@ -1,12 +1,10 @@
 package team24.issuetracker.domain;
 
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,9 +19,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToMany(mappedBy = "user")
-    private List<Assignee> assignees;
 
     private String name;
     private String email;
