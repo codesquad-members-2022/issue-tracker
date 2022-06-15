689c7d0531d5336d18b3a6d8f9e76d15f67b228d
sanghee <anshee97@naver.com>
2022-06-15 17:55:03 +0900

    Feat: PR 목록 화면 뷰모델 추가 (BumgeunSong/PR-tracker#3)

---
+++ PRTracker/Source/PR_Listing/ViewModel/LabelViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LabelViewModel.swift
+//  PRTracker
+//
+//  Created by 안상희 on 2022/06/15.
+//
+
+import Foundation
+
+final class LabelViewModel {
+    let name: Observable<String?> = Observable(nil)
+}
