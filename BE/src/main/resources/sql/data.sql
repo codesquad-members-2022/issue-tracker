f024701bbc73e3539ada28df38c85ce627f9b46b
WooJin Park(Ader) <ak2j38@gmail.com>
2022-06-15 16:56:17 +0900

    ✅ IssueService에 대한 테스트 추가 및 data.sql 추가
    o-authored-by: CMSSKKK <cms0501@gmail.com>

---
+++ BE/src/main/resources/sql/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO member(user_id, avatar_url) VALUES ('ron2', 'asdfasdf.com');
--- BE/src/test/java/com/ron2ader/issuetracker/service/IssueServiceTest.java
+++ BE/src/test/java/com/ron2ader/issuetracker/service/IssueServiceTest.java
@@ -2,14 +2,17 @@ package com.ron2ader.issuetracker.service;
 
 import com.ron2ader.issuetracker.controller.issuedto.IssueCreateRequest;
 import com.ron2ader.issuetracker.controller.issuedto.IssueDetailResponse;
+import com.ron2ader.issuetracker.controller.issuedto.IssueSimpleResponse;
 import com.ron2ader.issuetracker.controller.memberdto.MemberDto;
-import com.ron2ader.issuetracker.domain.issue.Issue;
-import com.ron2ader.issuetracker.domain.member.Member;
 import com.ron2ader.issuetracker.domain.member.MemberRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,36 +28,51 @@ class IssueServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
-    Member member;
-    Issue issue;
+    List<IssueCreateRequest> issues;
     IssueCreateRequest issueCreateRequest;
     MemberDto memberDto;
 
     @BeforeEach
     @Transactional
     void setUp() {
-        member = memberRepository.save(Member.of("ader", "www.github.com"));
-        memberDto = new MemberDto("ader", "www.github.com");
-        issue = Issue.of(member, "title", "contents", "url");
+        memberDto = new MemberDto("ron2", "asdfasdf.com");
         issueCreateRequest = new IssueCreateRequest("title", "contents", null);
+
+        issues = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+             issues.add(new IssueCreateRequest("title" + i, "contents" + i, null));
+        }
     }
 
     @Test
     @Transactional
-    void registerIssue() {
+    void registerIssueTest() {
         IssueDetailResponse issueDetailResponse = issueService.registerIssue(issueCreateRequest, memberDto);
 
-        assertThat(issueDetailResponse.getMemberDto().getUserId()).isEqualTo("ader");
-        assertThat(issueDetailResponse.getMemberDto().getAvatarUrl()).isEqualTo("www.github.com");
+        assertThat(issueDetailResponse.getMemberDto().getUserId()).isEqualTo("ron2");
+        assertThat(issueDetailResponse.getMemberDto().getAvatarUrl()).isEqualTo("asdfasdf.com");
         assertThat(issueDetailResponse.getIssueDetail().getTitle()).isEqualTo("title");
-
     }
 
     @Test
-    void findByIssueNumber() {
+    @Transactional
+    void findByIdTest() {
+        IssueDetailResponse issueDetailResponse = issueService.registerIssue(issueCreateRequest, memberDto);
+        IssueDetailResponse findIssue = issueService.findById(issueDetailResponse.getIssueDetail().getId());
+
+        assertThat(findIssue).isEqualTo(issueDetailResponse);
     }
 
     @Test
-    void findAllByOpenStatus() {
+    @Transactional
+    void findAllByOpenStatusPagingTest() {
+        for (int i = 0; i < 10; i++) {
+            issueService.registerIssue(issues.get(i), memberDto);
+        }
+
+        Page<IssueSimpleResponse> allByOpenStatus = issueService.findAllByOpenStatus(PageRequest.of(0, 5), true);
+
+        assertThat(allByOpenStatus.getTotalElements()).isEqualTo(10);
+        assertThat(allByOpenStatus.getSize()).isEqualTo(5);
     }
 }
