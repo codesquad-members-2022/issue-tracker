388929c6c35dd455ce1efd4480b9ddb8a760f5c9
Jihee hwang <hwangjihee94@gmail.com>
2022-06-15 10:20:21 +0900

    add: 컬러 추가

---
+++ ios/IssueTracker/IssueTracker/Presentation/Utils/Extensions/Extension+UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+UIColor.swift
+//  IssueTracker
+//
+//  Created by Jihee hwang on 2022/06/15.
+//
+
+import UIKit
+
+extension UIColor {
+    static let labelGray = UIColor(named: "labelGray")
+    static let lightBlue = UIColor(named: "lightBlue")
+    static let primary = UIColor(named: "primary")
+    static let secondary2 = UIColor(named: "secondary2")
+    static let secondary3 = UIColor(named: "secondary3")
+}
