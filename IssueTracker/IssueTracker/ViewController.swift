08279b6bfaeba44edc1aa3fd33b21bbdb7588cb6
Jinsujin <sylpid003@gmail.com>
2022-06-15 16:38:44 +0900

    [Jinsujin/issue-tracker#9] Feat: 버튼 UI 만들고, github login 연결하기

--- IssueTracker/IssueTracker/ViewController.swift
+++ IssueTracker/IssueTracker/ViewController.swift
@@ -6,17 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
-    override func viewDidAppear(_ animated: Bool) {
-        requestLogin()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        self.view.backgroundColor = .white
+        setupView()
     }
+    
+    private func setupView() {
+        view.addSubview(loginButton)
+        loginButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(type: .custom, primaryAction: UIAction(handler: { _ in
+            self.requestLogin()
+        }))
+        button.setTitle("Github 로그인", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        return button
+    }()
     
     private func requestLogin() {
         if let url = URL(string: "https://github.com/login/oauth/authorize?client_id=\(PrivateStorage.clientId)") {
