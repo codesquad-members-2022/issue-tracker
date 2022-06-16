5088f63146e83c0bb3b876aba2034033c2413172
Sangbeom Heo <mansaout@gmail.com>
2022-06-16 11:20:41 +0900

    Feat: 버튼 컴포넌트의 abled, disAbled 버튼 스타일 생성

---
+++ FE/src/common/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { COLORS, GREYSCALE } from '@/constants';
+
+function Button({ isDisabled = false }) {
+  const Box = isDisabled ? ButtonBoxDisabled : ButtonBoxAbled;
+
+  return <Box>dd</Box>;
+}
+
+const ButtonBox = styled.button`
+  width: 340px;
+  height: 64px;
+  background-color: ${COLORS.BLUE};
+  color: ${GREYSCALE.OFF_WHITE};
+  border-radius: 20px;
+  ${({ theme }) => theme.TYPOGRAPHY.LINK_MEDIUM};
+`;
+
+const ButtonBoxAbled = styled(ButtonBox)`
+  &:hover {
+    background-color: ${COLORS.DARK_BLUE};
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 4px ${COLORS.LIGHT_BLUE};
+  }
+`;
+
+const ButtonBoxDisabled = styled(ButtonBox)`
+  opacity: 0.5;
+`;
+
+export default Button;
