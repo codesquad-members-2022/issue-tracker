69a27ba6eade78cbe4d2fa1ddcd4acc8492f6a99
sanghee <dev.sanghee@gmail.com>
2022-06-17 03:52:21 +0900

    Design: 메인 NavigationBarButton에 이미지 추가 (BumgeunSong/PR-tracker#3)

--- PRTracker/Source/PR_Listing/View/ListingViewController.swift
+++ PRTracker/Source/PR_Listing/View/ListingViewController.swift
@@ -11,5 +11,42 @@ class ListingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureSearchController()
+        configureNavigationBar()
+    }
+    
+    private func configureSearchController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        
+        searchController.searchBar.showsCancelButton = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func configureNavigationBar() {
+        let button = CustomBarButton()
+        let leftBarButtonItem = UIBarButtonItem(customView: button.left)
+        let rightBarButtonItem = UIBarButtonItem(customView: button.right)
+        self.navigationItem.leftBarButtonItem = leftBarButtonItem
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+        self.navigationItem.title = "PR"
+    }
+}
+
+
+extension ListingViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        dump(searchController.searchBar.text)
+    }
+}
+
+extension ListingViewController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        return true
     }
 }
