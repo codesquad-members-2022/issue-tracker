740a46998a7314bdef9d60842f957de08d72c37f
P_SeoB <akdlsjtm21@naver.com>
2022-06-16 13:19:08 +0900

    [chore] lint 재설정

---
+++ IssueTracker15/IssueTracker15/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  IssueTracker15
+//
+//  Created by 박진섭 on 2022/06/16.
+//
+
+import Foundation
---
+++ IssueTracker15/IssueTracker15/OAuthLoginButton.swift
@@ -0,0 +1,8 @@
+//
+//  OAuthLoginButton.swift
+//  IssueTracker15
+//
+//  Created by 박진섭 on 2022/06/16.
+//
+
+import Foundation
