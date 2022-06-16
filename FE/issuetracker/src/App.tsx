5ce1c03bb830fac7f91f8e8a1fc87916aed00fa5
SeungHyun <fm10033@gmail.com>
2022-06-16 11:35:16 +0900

    Design : IssueList index icon 적용

--- FE/issuetracker/src/App.tsx
+++ FE/issuetracker/src/App.tsx
@@ -3,6 +3,7 @@ import { theme } from 'design/theme';
 import { BrowserRouter } from 'react-router-dom';
 import Router from 'Router';
 import GlobalStyles from 'design/GlobalStyles';
+import 'assets/style.css';
 
 function App() {
   return (
