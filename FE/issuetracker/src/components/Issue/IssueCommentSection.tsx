8f1a3e2b367d73c87ae589ef8f7c6f599b104542
soralee <sora2821@gmail.com>
2022-06-17 03:15:54 +0900

    Feat: Input Comment 컴포넌트 구현

--- FE/issuetracker/src/components/Issue/IssueCommentSection.tsx
+++ FE/issuetracker/src/components/Issue/IssueCommentSection.tsx
@@ -1,5 +1,6 @@
 import * as S from 'components/Issue/styled.issueCommentSection';
 import IssueComment from 'components/Issue/IssueComment';
+import InputComment from 'components/Issue/InputComment';
 import userImageURL from 'assets/images/UserImageLarge.svg';
 import userImageURL2 from 'assets/images/UserImageLarge2.svg';
 
@@ -8,6 +9,7 @@ function IssueCommentSection() {
     <S.commentSection>
       <IssueComment imageURL={userImageURL} />
       <IssueComment imageURL={userImageURL2} />
+      <InputComment />
     </S.commentSection>
   );
 }
