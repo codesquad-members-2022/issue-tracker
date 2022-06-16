076223a6f08af8616f576ea5efd65df941017c58
nanjeong <whdwkxj7@naver.com>
2022-06-16 17:51:09 +0900

    Chore: .eslintrc value 숫자에서  영문으로 변경

--- FE/.eslintrc.js
+++ FE/.eslintrc.js
@@ -10,19 +10,16 @@ module.exports = {
     'prettier/@typescript-eslint',
   ],
   rules: {
-    'linebreak-style': 0,
-    'import/prefer-default-export': 0,
-    'prettier/prettier': 0,
-    'import/extensions': 0,
-    'no-use-before-define': 0,
-    'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
-    'no-shadow': 0,
-    'react/prop-types': 0,
+    'linebreak-style': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'off', // prettier 규칙을 지키지 않았을 때, lint 에러로 만들 것인지
+    'import/extensions': 'off', // import 할 때, 확장자명을 적을 것인지
+    'import/no-unresolved': 'off', // 경로가 잘못됐을 때 에러 만들 것인지
+    'import/no-extraneous-dependencies': 'off', // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
   },
 };
