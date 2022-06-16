74a4fe674475a550baa7feae39a6cd8c57668bb7
Sangbeom Heo <mansaout@gmail.com>
2022-06-16 10:29:20 +0900

    Style: 절대경로 설정
    - 같은 깊이 에 있어서 './'만 쓸 수있는 경우는 상대경로 사용
    - 나머지는 절대경로 사용

--- FE/src/assets/icons/Icon.tsx
+++ FE/src/assets/icons/Icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as icons from './svgs';
+import { icons } from '@/assets/icons/svgs';
 
 const Icon = ({ iconName, width = 16, height = 16 }) => {
   const IconSvg = icons[iconName];
