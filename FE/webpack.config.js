816cf1626f0d0c854edc588159509c1e584786d2
nanjeong <whdwkxj7@naver.com>
2022-06-16 17:06:49 +0900

    Comment: GlobalStyles.tsx 파일과 webpack.config.js 파일에 출처 주석 추가

--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -1,3 +1,4 @@
+// NOTE: 참고 링크 https://velog.io/@jjunyjjuny/React-TS-boilerplate-%EC%A0%9C%EC%9E%91%EA%B8%B0-%ED%99%98%EA%B2%BD-%EA%B5%AC%EC%84%B1
 const dotenv = require('dotenv');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
