6cfce7a5004b900d419623028fa6b0f37af3d395
soralee <sora2821@gmail.com>
2022-06-17 00:36:49 +0900

    Feat: Issue 컴포넌트 UI 구현 완성

--- FE/issuetracker/src/components/Issue/IssueSidebar.tsx
+++ FE/issuetracker/src/components/Issue/IssueSidebar.tsx
@@ -5,35 +5,45 @@ import userImageURL2 from 'assets/images/UserImageLarge2.svg';
 
 function IssueSidebar() {
   return (
-    <S.sidebar>
-      <S.userBar>
-        <S.barHeader>
-          <S.barHeaderText>작성자</S.barHeaderText>
-          <I.plus />
-        </S.barHeader>
-        <S.userUnit>
-          <img src={userImageURL} alt={userImageURL} />
-          <S.userName>Oni</S.userName>
-        </S.userUnit>
-        <S.userUnit>
-          <img src={userImageURL2} alt={userImageURL2} />
-          <S.userName>Daniel</S.userName>
-        </S.userUnit>
-      </S.userBar>
-      <S.labelBar>
-        <S.barHeader>
-          <S.barHeaderText>레이블</S.barHeaderText>
-          <I.plus />
-        </S.barHeader>
-        <S.label>documentation</S.label>
-      </S.labelBar>
-      <S.milestoneBar>
-        <S.barHeader>
-          <S.barHeaderText>마일스톤</S.barHeaderText>
-          <I.plus />
-        </S.barHeader>
-      </S.milestoneBar>
-    </S.sidebar>
+    <S.sidebarWrapper>
+      <S.sidebar>
+        <S.userBar>
+          <S.barHeader>
+            <S.barHeaderText>작성자</S.barHeaderText>
+            <I.plus />
+          </S.barHeader>
+          <S.userUnit>
+            <img src={userImageURL} alt={userImageURL} />
+            <S.userName>Oni</S.userName>
+          </S.userUnit>
+          <S.userUnit>
+            <img src={userImageURL2} alt={userImageURL2} />
+            <S.userName>Daniel</S.userName>
+          </S.userUnit>
+        </S.userBar>
+        <S.labelBar>
+          <S.barHeader>
+            <S.barHeaderText>레이블</S.barHeaderText>
+            <I.plus />
+          </S.barHeader>
+          <S.label>documentation</S.label>
+        </S.labelBar>
+        <S.milestoneBar>
+          <S.barHeader>
+            <S.barHeaderText>마일스톤</S.barHeaderText>
+            <I.plus />
+          </S.barHeader>
+          <S.progressBar />
+          <S.milstoneContent>마스터즈 코스</S.milstoneContent>
+        </S.milestoneBar>
+      </S.sidebar>
+      <S.buttonWrapper>
+        <S.deleteButton>
+          <I.trash />
+          <S.deleteButtonText>이슈 삭제</S.deleteButtonText>
+        </S.deleteButton>
+      </S.buttonWrapper>
+    </S.sidebarWrapper>
   );
 }
 export default IssueSidebar;
--- FE/issuetracker/src/components/Issue/styled.issueSidebar.js
+++ FE/issuetracker/src/components/Issue/styled.issueSidebar.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { mixin } from 'design/GlobalStyles';
 
-export const sidebar = styled.aside`
+export const sidebarWrapper = styled.aside``;
+
+export const sidebar = styled.div`
   width: 308px;
   height: 504px;
   border-radius: 16px;
@@ -56,8 +58,44 @@ export const label = styled.label`
   width: 121px;
   height: 28px;
   padding: 0 16px;
-  margin: 16px 0;
   border-radius: 30px;
+  margin: 16px 0;
   background: ${({ theme }) => theme.backgroundColors.purple1};
   color: ${({ theme }) => theme.fontColors.gray1};
+`;
+
+export const progressBar = styled.div`
+  height: 8px;
+  border-radius: 10px;
+  margin-top: 16px;
+  background: linear-gradient(
+    90deg,
+    ${({ theme }) => theme.backgroundColors.blue2} 0%,
+    ${({ theme }) => theme.backgroundColors.blue2} 15.21%,
+    ${({ theme }) => theme.backgroundColors.gray3} 15.22%,
+    ${({ theme }) => theme.backgroundColors.gray3} 100%
+  );
+`;
+
+export const milstoneContent = styled.div`
+  ${({ theme }) => theme.fontStyles.textSmall};
+  height: 24px;
+  margin-top: 4px;
+  color: ${({ theme }) => theme.fontColors.gray2};
+`;
+
+export const buttonWrapper = styled.div`
+  ${mixin.flexbox({ dir: 'row-reverse' })};
+  height: 32px;
+`;
+
+export const deleteButton = styled.button.attrs(() => ({ type: 'button' }))`
+  width: 67px;
+  height: 32px;
+  margin: 12px 32px 0 0;
+  color: ${({ theme }) => theme.fontColors.red};
+`;
+
+export const deleteButtonText = styled.span`
+  ${({ theme }) => theme.fontStyles.linkSSmall};
 `;
