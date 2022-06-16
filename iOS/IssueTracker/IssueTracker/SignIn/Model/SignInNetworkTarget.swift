2693553ca64e65eed5c2c3fcd2ba9a3a94ca4500
Sol <hskim6521@naver.com>
2022-06-16 17:47:52 +0900

    feat: NetworkTarget을 추상화한 NetworkTargetable 추상 타입 추가 (bugpigg/issue-tracker#10)

--- iOS/IssueTracker/IssueTracker/SignIn/Model/SignInNetworkTarget.swift
+++ iOS/IssueTracker/IssueTracker/SignIn/Model/SignInNetworkTarget.swift
@@ -12,20 +12,7 @@ enum SignInNetworkTarget {
     case requestAccessToken(clientID: String, clientSecret: String, code: String)
 }
 
-extension SignInNetworkTarget {
-    private var baseURL: String {
-        return "https://github.com/login/oauth"
-    }
-
-    private var path: String {
-        switch self {
-        case .requestCode:
-            return "/authorize"
-        case .requestAccessToken:
-            return "/access_token"
-        }
-    }
-
+extension SignInNetworkTarget: NetworkTargetable {
     var queryItem: [URLQueryItem] {
         switch self {
         case .requestCode(let clientID):
@@ -58,6 +45,22 @@ extension SignInNetworkTarget {
             return false
         case .requestAccessToken:
             return true
+        }
+    }
+}
+
+// MARK: - private computed properties
+private extension SignInNetworkTarget {
+    private var baseURL: String {
+        return "https://github.com/login/oauth"
+    }
+
+    private var path: String {
+        switch self {
+        case .requestCode:
+            return "/authorize"
+        case .requestAccessToken:
+            return "/access_token"
         }
     }
 }
