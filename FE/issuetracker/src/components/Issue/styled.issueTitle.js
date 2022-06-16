8fdaa08a1d81d48334bca77d9c72b3eab9d16d3a
soralee <sora2821@gmail.com>
2022-06-16 17:57:15 +0900

    Feat: issue Title 컴포넌트 구현

---
+++ FE/issuetracker/src/components/Issue/styled.issueTitle.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { mixin } from 'design/GlobalStyles';
+
+export const issueTitleWrapper = styled.div`
+  ${mixin.flexbox({ vertical: 'center' })};
+  position: relative;
+`;
+
+export const issueTitle = styled.div`
+  ${({ theme }) => theme.fontStyles.display};
+  color: ${({ theme }) => theme.fontColors.gray5};
+  margin-right: 16px;
+`;
+
+export const issueNumber = styled.div`
+  ${({ theme }) => theme.fontStyles.display};
+  color: ${({ theme }) => theme.fontColors.gray2};
+`;
+
+export const buttonWrapper = styled.div`
+  display: inline-flex;
+  position: absolute;
+  right: 0;
+`;
