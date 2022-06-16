db5a99c4ee2bf41cdefa85bfd17de98f7c849a04
soralee <sora2821@gmail.com>
2022-06-16 17:56:38 +0900

    Feat: issue 컴포넌트 구현

---
+++ FE/issuetracker/src/components/Issue/index.tsx
@@ -0,0 +1,21 @@
+import * as S from 'components/Issue/styled.index';
+import IssueTitle from 'components/Issue/IssueTitle';
+import IssueDetail from 'components/Issue/IssueDetail';
+import IssueComments from 'components/Issue/IssueCommentSection';
+import IssueSidebar from 'components/Issue/IssueSidebar';
+
+function Issue() {
+  return (
+    <>
+      <IssueTitle />
+      <IssueDetail />
+      <S.seperator />
+      <S.issueContentWrapper>
+        <IssueComments />
+        <IssueSidebar />
+      </S.issueContentWrapper>
+    </>
+  );
+}
+
+export default Issue;
