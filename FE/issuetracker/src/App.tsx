4c4cbc62911e820912237bfabec15f1f6783e03e
SeungHyun <fm10033@gmail.com>
2022-06-17 02:11:57 +0900

    Refact : Header 컴포넌트 위치 변경

--- FE/issuetracker/src/App.tsx
+++ FE/issuetracker/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header />
       <BrowserRouter>
+        <Header />
         <Router />
       </BrowserRouter>
     </ThemeProvider>
