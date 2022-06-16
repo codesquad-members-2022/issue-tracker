3402658f22b46b939721257f0cf7ecb66c06113b
jindonyy <rsefa94@gmail.com>
2022-06-16 20:28:06 +0900

    Feat: useInputText 매개변수 옵셔널로 수정
    closed jindonyy/issue-tracker/#9

--- FE/src/hooks/useInputTextValue.tsx
+++ FE/src/hooks/useInputTextValue.tsx
@@ -4,12 +4,12 @@ import debounce from '@/utils/debounce';
 export default function useInputTextValue() {
   const [inputValue, setInputValue] = useState('');
 
-  const updateInputValue = (event: React.ChangeEvent<HTMLInputElement>, ms: number) => {
-    debounce(() => setInputValue(event.target.value), ms);
+  const updateInputValue = (event: React.ChangeEvent<HTMLInputElement>, ms?: number) => {
+    debounce(() => setInputValue(event.target.value), ms ? ms : 0);
   };
 
-  const checkInputValueLength = (min: number, max: number) =>
-    min <= inputValue.length && max >= inputValue.length;
+  const checkInputValueLength = (min?: number, max?: number) =>
+    (min ? min : 0) <= inputValue.length && (max ? max : Infinity) >= inputValue.length;
 
   return { inputValue, setInputValue, updateInputValue, checkInputValueLength };
 }
