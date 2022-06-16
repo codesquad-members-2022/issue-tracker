57ddb8b9bbdf5e007ced83bc3d3d184ca99e0bee
SeungHyun <fm10033@gmail.com>
2022-06-17 02:12:59 +0900

    React : 헤더 url 체크 방법 변경

--- FE/issuetracker/src/components/header/index.tsx
+++ FE/issuetracker/src/components/header/index.tsx
@@ -2,7 +2,7 @@ import * as S from 'components/header/styled.index';
 import AccountSrc from 'assets/images/UserImageLarge.svg';
 
 function Header() {
-  const isLoginPage = window.location.href === `http://localhost:3000/` ? true : false;
+  const isLoginPage = window.location.href === `http://localhost:3000/`;
   return (
     <S.HeaderWrap checkPage={isLoginPage}>
       <S.Title to="/">Issue Tracker</S.Title>
