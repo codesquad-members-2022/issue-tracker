5916f251dba16338341ff1bacf438abab7efeda9
soralee <sora2821@gmail.com>
2022-06-16 22:58:41 +0900

    Refactor: eslint no-unneeded-ternary error 수정

--- FE/issuetracker/src/components/header/index.tsx
+++ FE/issuetracker/src/components/header/index.tsx
@@ -2,7 +2,7 @@ import * as S from 'components/header/styled.index';
 import AccountSrc from 'assets/images/UserImageLarge.svg';
 
 function Header() {
-  const isLoginPage: boolean = window.location.href === `http://localhost:3000/` ? true : false;
+  const isLoginPage: boolean = window.location.href === `http://localhost:3000/`;
   return (
     <S.HeaderWrap checkPage={isLoginPage}>
       <S.Title>Issue Tracker</S.Title>
