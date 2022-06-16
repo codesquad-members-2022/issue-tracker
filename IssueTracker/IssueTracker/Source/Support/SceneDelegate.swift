2f77307e1eae510ed6141afed874686d2c994358
asqw887 <asqw887@gmail.com>
2022-06-16 18:19:22 +0900

    Feat: access token 요청 로직 구현

--- IssueTracker/IssueTracker/Source/Support/SceneDelegate.swift
+++ IssueTracker/IssueTracker/Source/Support/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-//        window?.rootViewController = TabBarViewController()
+        
+        //TODO: Access token 유효성 검사 후 화면 흐름 제어하기
+
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
     }
@@ -27,35 +29,55 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let targetUrl: IssueTrackerTarget = .requestAccessToken(code: authorizedCode)
         print(targetUrl)
 
-        let validURL = IssueTrackerTarget.requestAccessToken(code: authorizedCode)
-//        guard let requestURL = createRequest(validURL) else { return }
+        // authorizedCode를 넣은 IssueTarget생성
+        // 얘로 validURL을 만들어서 POST 요청을 보내야함
+        let target = IssueTrackerTarget.requestAccessToken(code: authorizedCode)
 
-//        URLSession.shared.dataTask(with: requestURL) { data, _, _ in
-//
-//            if let data = data {
-//                print(data)
-//            }
-//
-//        }.resume()
+        guard let requestURL = createRequest(target) else { return }
+
+        URLSession.shared.dataTask(with: requestURL) { data, _, error in
+
+            guard error == nil else { return }
+            guard let data = data else { return }
+            guard let jsonData = try! JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+
+            //TODO: access Token 저장 방식 변경
+            UserDefaults.standard.set("access_token", forKey: jsonData["access_token"] as! String)
+            
+            DispatchQueue.main.async {
+                self.window?.rootViewController = TabBarViewController()
+            }
+
+        }.resume()
+
     }
 
     private func createRequest(_ target: BaseTarget) -> URLRequest? {
-        guard let baseUrl = target.baseURL else {
+        // base url
+        guard var url = target.baseURL else {
             return nil
         }
-
-        var url = baseUrl
+        // parameter append
         if let path = target.path {
-            url = baseUrl.appendingPathComponent(path)
+            url = url.appendingPathComponent(path)
+        }
+        var request = URLRequest(url: url)
+
+        // paramter를 Json으로 encode
+        if let param = target.parameter {
+            let requestBody = try! JSONSerialization.data(withJSONObject: target.parameter!, options: .init())
+            request.httpBody = requestBody
         }
 
-        var request = URLRequest(url: url)
-        request.httpMethod = target.method.value
+        // 우리가 전송하는 헤더의 타입 설정
+        request.setValue(target.content.value, forHTTPHeaderField: target.content.forHTTPHeaderField)
 
-        if let param = target.parameter,
-           let body = try? JSONSerialization.data(withJSONObject: param, options: .init()) {
-                request.httpBody = body
-            }
+        // 응답으로 받아오는 data 의 타입 설정
+        if let accept = target.accept {
+            request.addValue(accept.value, forHTTPHeaderField: accept.forHTTPHeaderField)
+        }
+
+        request.httpMethod = target.method.value
 
         return request
     }
