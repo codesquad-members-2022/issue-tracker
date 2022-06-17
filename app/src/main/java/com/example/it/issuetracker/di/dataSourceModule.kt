64449a4967f349c8a70269ba13e2da19acc01b33
stitch <skaxodn97@gmail.com>
2022-06-17 13:00:41 +0900

    feat: LabelDataSource DI 추가
    - 의존성 주입
    LabelDataSource DI를 추가

--- app/src/main/java/com/example/it/issuetracker/di/dataSourceModule.kt
+++ app/src/main/java/com/example/it/issuetracker/di/dataSourceModule.kt
@@ -2,8 +2,11 @@ package com.example.it.issuetracker.di
 
 import com.example.it.issuetracker.data.datasource.IssueTrackerDataSource
 import com.example.it.issuetracker.data.datasource.IssueTrackerDefaultDataSource
+import com.example.it.issuetracker.data.datasource.LabelDataSource
+import com.example.it.issuetracker.data.datasource.LabelDataSourceDefaultImpl
 import org.koin.dsl.module
 
 val dataSourceModule = module {
     single<IssueTrackerDataSource> { IssueTrackerDefaultDataSource() }
+    single<LabelDataSource> { LabelDataSourceDefaultImpl(get()) }
 }
