8145bb404e1e7e23ef9edb2dbda3f76a4d418ce8
taewoo <66770613+bn-tw2020@users.noreply.github.com>
2022-06-17 13:02:02 +0900

    Delete dataSourceModule.kt

--- app/src/main/java/com/example/it/issuetracker/di/dataSourceModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.it.issuetracker.di
-
-import com.example.it.issuetracker.data.datasource.IssueTrackerDataSource
-import com.example.it.issuetracker.data.datasource.IssueTrackerDefaultDataSource
-import org.koin.dsl.module
-
-val dataSourceModule = module {
-    single<IssueTrackerDataSource> { IssueTrackerDefaultDataSource() }
-}
