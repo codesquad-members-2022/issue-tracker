89bae8c046db72f7ba8a1eec59ef647aa9208296
sanghee <dev.sanghee@gmail.com>
2022-06-17 23:41:39 +0900

    Chore: 임시로 이메일 로그인 버튼 클릭 시 메인으로 이동하도록 구현 (BumgeunSong/PR-tracker#3)

--- PRTracker/Source/Login/View/ViewController.swift
+++ PRTracker/Source/Login/View/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var githubLoginButton: UIButton!
     @IBOutlet weak var appleLoginButton: UIButton!
     
+    @IBAction func emaliLoginButtonTapped(_ sender: Any) {
+        // 임시적으로 이메일 로그인 버튼 클릭 시 메인 화면으로 이동하도록 구현
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "loginSegue", sender: nil)
+        }
+    }
+    
     @IBAction func githubLoginButtonTapped(_ sender: Any) {
         // TODO: OAuthManager에게 인증 요청
         viewModel.requestGithubLogin()
@@ -19,10 +26,6 @@ class ViewController: UIViewController {
     
     @IBAction func appleLoginButtonTapped(_ sender: Any) {
         // TODO: OAuthManager에게 인증 요청
-        // 임시적으로 애플 로그인 버튼 클릭 시 메인 화면으로 이동하도록 구현
-        DispatchQueue.main.async {
-            self.performSegue(withIdentifier: "loginSegue", sender: nil)
-        }
     }
     
     private let viewModel: LoginViewModel = LoginViewModel()
