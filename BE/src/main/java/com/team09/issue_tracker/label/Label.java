ac21d82745be02e649b74c0ddadd7378fc08f078
mybloom <baejungeun11@gmail.com>
2022-06-18 13:17:48 +0900

    feat: Label 엔티티에 Dto생성 메서드 추가 (bugpigg/issue-tracker#8)

--- BE/src/main/java/com/team09/issue_tracker/label/Label.java
+++ BE/src/main/java/com/team09/issue_tracker/label/Label.java
@@ -1,10 +1,12 @@
 package com.team09.issue_tracker.label;
 
+import com.team09.issue_tracker.label.dto.LabelSelectResponseDto;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Label {
@@ -22,4 +24,15 @@ public class Label {
 
 	private String backgroundColor;
 
+
+
+	public LabelSelectResponseDto toResponseDto() {
+		return LabelSelectResponseDto.builder()
+			.id(id)
+			.title(title)
+			.description(description)
+			.backgroundColor(backgroundColor)
+			.isDarkMode(isDarkMode)
+			.build();
+	}
 }
