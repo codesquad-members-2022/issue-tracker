b11e358370237ca01c15251c91d9af18dc388d80
mybloom <baejungeun11@gmail.com>
2022-06-18 13:40:59 +0900

    refactor: Member와 연관관계를 삭제하고 memberId를 갖도록 수정  (bugpigg/issue-tracker#8)
    - Member의 객체까지 탐색할 필요가 없다고 판단해 삭제

--- BE/src/main/java/com/team09/issue_tracker/issue/Issue.java
+++ BE/src/main/java/com/team09/issue_tracker/issue/Issue.java
@@ -1,9 +1,11 @@
 package com.team09.issue_tracker.issue;
 
-import com.team09.issue_tracker.comment.Comment;
 import com.team09.issue_tracker.common.BaseTimeEntity;
-import com.team09.issue_tracker.member.Member;
+import com.team09.issue_tracker.issue.dto.IssueListResponseDto;
 import com.team09.issue_tracker.milestone.Milestone;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,7 +14,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 public class Issue extends BaseTimeEntity {
 
@@ -31,8 +41,7 @@ public class Issue extends BaseTimeEntity {
 	@JoinColumn(name = "milestone_id")
 	private Milestone milestone;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
-	private Member writer;
-	
+	private Long memberId;
+
+
 }
