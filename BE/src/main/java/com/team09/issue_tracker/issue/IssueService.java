5f2612839c08eec2b8ef041200b0d4bc07d2ec85
mybloom <baejungeun11@gmail.com>
2022-06-18 23:08:01 +0900

    feat: 이슈 생성(기본값만) 로직 작성  (bugpigg/issue-tracker#8)

--- BE/src/main/java/com/team09/issue_tracker/issue/IssueService.java
+++ BE/src/main/java/com/team09/issue_tracker/issue/IssueService.java
@@ -1,6 +1,9 @@
 package com.team09.issue_tracker.issue;
 
+import com.team09.issue_tracker.common.CommonResponseDto;
+import com.team09.issue_tracker.issue.dto.IssueSaveRequestDto;
 import com.team09.issue_tracker.issue.dto.IssueListResponseDto;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,22 @@ public class IssueService {
 		List<IssueListResponseDto> response = issues.stream().map(Issue::toResponseDto)
 			.collect(Collectors.toList());
 
+		return Collections.unmodifiableList(response);
+	}
+
+	@Transactional
+	public CommonResponseDto create(IssueSaveRequestDto issueCreateRequestDto, Long memberId) {
+		Long milestoneId = issueCreateRequestDto.getMilestoneId();
+		List<Long> labelIds = issueCreateRequestDto.getLabelIds();
+		List<Long> assigneeIds = issueCreateRequestDto.getAssigneeIds();
+		if (milestoneId != null && labelIds != null && assigneeIds != null) {
+			return null;
+		}
+
+		boolean isOpened = true;
+		Issue issue = issueRepository.save(
+			Issue.fromForMandatory(issueCreateRequestDto, isOpened, memberId));
+		CommonResponseDto response = issue.toCommonResponse();
 		return response;
 	}
 }
