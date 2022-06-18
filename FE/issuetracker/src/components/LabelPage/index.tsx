34d2e2dbcb63fa59c9df95f60a8bc5c8990e076b
soralee <sora2821@gmail.com>
2022-06-18 17:25:37 +0900

    Feat: label page UI 구현

--- FE/issuetracker/src/components/LabelPage/index.tsx
+++ FE/issuetracker/src/components/LabelPage/index.tsx
@@ -1,27 +1,13 @@
-import * as I from 'design/icons';
-import * as S from 'components/LabelPage/styled.index';
+import LabelTop from 'components/LabelPage/LabelTop';
+import LabelList from 'components/LabelPage/LabelList';
 
 function LabelPage() {
   const isLabelPage = window.location.href === `http://localhost:3000/label`;
   return (
-    <S.labelTop>
-      <S.tabBar>
-        <S.leftBar to="/label" isLabelPage={isLabelPage}>
-          <I.tag />
-          <S.LinkText>레이블</S.LinkText>
-          <S.numberText>(3)</S.numberText>
-        </S.leftBar>
-        <S.rightBar to="/milestone">
-          <I.milestone />
-          <S.LinkText>마일스톤</S.LinkText>
-          <S.numberText>(2)</S.numberText>
-        </S.rightBar>
-      </S.tabBar>
-      <S.addButton>
-        <I.plus />
-        <S.addButtonText>추가</S.addButtonText>
-      </S.addButton>
-    </S.labelTop>
+    <>
+      <LabelTop activeLabel={isLabelPage} />
+      <LabelList />
+    </>
   );
 }
 export default LabelPage;
