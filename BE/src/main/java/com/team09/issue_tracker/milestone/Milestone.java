de9e02e6a59ee65c59dfb4fef3d53b8b26651fb7
mybloom <baejungeun11@gmail.com>
2022-06-18 13:45:45 +0900

    feat: Milestone 엔티티에 Member 연관관계가 빠져 있어서 설정, getTitle() 메서드 생성 (bugpigg/issue-tracker#8)
    - 이슈 목록 조회시 마일스톤의 title을 조회할 수 있도록 필요 컬럼에만 getter생성

--- BE/src/main/java/com/team09/issue_tracker/milestone/Milestone.java
+++ BE/src/main/java/com/team09/issue_tracker/milestone/Milestone.java
@@ -1,11 +1,15 @@
 package com.team09.issue_tracker.milestone;
 
+import com.team09.issue_tracker.member.Member;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Milestone {
@@ -21,4 +25,11 @@ public class Milestone {
 
 	private LocalDate completionDate;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member writer;
+
+	public String getTitle() {
+		return title;
+	}
 }
