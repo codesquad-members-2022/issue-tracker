f7f7d29d107c818efce994a754a78a620a5870be
mybloom <baejungeun11@gmail.com>
2022-06-18 14:01:44 +0900

    fix: ResponseDto에서 Label 엔티티 대신 LabelDto를 사용 하도록 수정, 불필요한 생성자 삭제(bugpigg/issue-tracker#8)

---
+++ BE/src/main/java/com/team09/issue_tracker/issue/dto/IssueListResponseDto.java
@@ -0,0 +1,21 @@
+package com.team09.issue_tracker.issue.dto;
+
+import com.team09.issue_tracker.label.dto.LabelSelectResponseDto;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class IssueListResponseDto {
+
+	private Long id;
+	private String title;
+	private String content;
+	private String milestoneTitle;
+	private List<LabelSelectResponseDto> labels;
+
+}
