2639e413a05165fc78448afdda5bd23e3f281dea
mybloom <baejungeun11@gmail.com>
2022-06-18 13:22:50 +0900

    feat: 이슈 목록 조회시 Issue엔티티에서 IssueLabel 객체를 사용하기 위해서 getter 생성 (bugpigg/issue-tracker#8)
    - TODO : 해당부분 getter를 사용하지 않고 해결 방법 찾아보기

--- BE/src/main/java/com/team09/issue_tracker/issue/IssueLabel.java
+++ BE/src/main/java/com/team09/issue_tracker/issue/IssueLabel.java
@@ -9,7 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.Getter;
 
+@Getter
 @Entity
 public class IssueLabel {
 
