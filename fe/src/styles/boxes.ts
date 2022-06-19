e29289e7c11410bdbbc1e343ce53779cb7817c90
moonyerim2 <moonyerim2@gmail.com>
2022-06-20 04:43:07 +0900

    feat: LargeLabel 컴포넌트 구현

--- fe/src/styles/boxes.ts
+++ fe/src/styles/boxes.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface FlexCenterBoxProps {
   gap?: number;
@@ -24,4 +24,27 @@ export const IconBox = styled(FlexCenterBox)<IconBoxProps>`
     width: ${({ width }) => width}px;
     height: ${({ height }) => height}px;
   }
+`;
+
+interface LabelBoxProps {
+  style: {
+    padding: string;
+    fontWeight: string;
+    fontSize: string;
+    borderRadius: number;
+  };
+}
+
+export const LabelBox = styled(FlexCenterBox)<LabelBoxProps>`
+  ${({ theme: { fontSize, fontWeight }, style }) => {
+    const { padding, fontWeight: ftWeight, fontSize: ftSize, borderRadius } = style;
+
+    return css`
+      display: inline-flex;
+      padding: ${padding};
+      font-weight: ${fontWeight[ftWeight]};
+      font-size: ${fontSize[ftSize]};
+      border-radius: ${borderRadius}px;
+    `;
+  }}
 `;
