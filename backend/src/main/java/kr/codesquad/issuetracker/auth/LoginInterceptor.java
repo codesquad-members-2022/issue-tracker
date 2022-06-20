77876c30ae2c7407fc335d43c7625a8342736809
Jaehong Choe <qnalsfj@gmail.com>
2022-06-21 02:28:37 +0900

    feat: 클라이언트 요청시 넘어온 토큰에서 userId 추출

--- backend/src/main/java/kr/codesquad/issuetracker/auth/LoginInterceptor.java
+++ backend/src/main/java/kr/codesquad/issuetracker/auth/LoginInterceptor.java
@@ -1,11 +1,14 @@
 package kr.codesquad.issuetracker.auth;
 
 import io.jsonwebtoken.Claims;
+import java.util.NoSuchElementException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import kr.codesquad.issuetracker.auth.exception.AuthHeaderNotFoundException;
 import kr.codesquad.issuetracker.auth.exception.IllegalJwtFormatException;
 import kr.codesquad.issuetracker.auth.jwt.JWTHandler;
+import kr.codesquad.issuetracker.core.user.User;
+import kr.codesquad.issuetracker.core.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
@@ -16,21 +19,31 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final JWTHandler jwtHandler;
+    private final UserRepository userRepository;
+
+    private static final String BEARER = "Bearer";
+    private static final String USER_ID = "userId";
+
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
-        String userId = getUserId(request);
 
-        return false;
+        String userId = extractUserId(request);
+        User user = userRepository.findByGitHubId(userId)
+                .orElseThrow(() -> new NoSuchElementException());
+
+        request.setAttribute(USER_ID, user.getUserId());
+
+        return true;
     }
 
-    private String getUserId(HttpServletRequest request)  {
+    private String extractUserId(HttpServletRequest request)  {
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         verifyHeader(authHeader);
 
-        Claims claims = jwtHandler.decodeJwt(authHeader.split("Bearer ")[1]);
-        return claims.get("userId", String.class);
+        Claims claims = jwtHandler.decodeJwt(authHeader.split(" ")[1]);
+        return claims.get(USER_ID, String.class);
     }
 
 
@@ -38,7 +51,7 @@ public class LoginInterceptor implements HandlerInterceptor {
         if (authorization.trim().isEmpty() || authorization == null) {
             throw new AuthHeaderNotFoundException();
         }
-        if (!authorization.startsWith("Bearer")) {
+        if (!authorization.startsWith(BEARER)) {
             throw new IllegalJwtFormatException("Authorization 헤더는 'Bearer'로 시작해야 합니다.");
         }
     }
--- backend/src/main/java/kr/codesquad/issuetracker/core/user/UserRepository.java
+++ backend/src/main/java/kr/codesquad/issuetracker/core/user/UserRepository.java
@@ -1,8 +1,11 @@
 package kr.codesquad.issuetracker.core.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUserId(String userId);
+
+    Optional<User> findByGitHubId(String userId);
 }
