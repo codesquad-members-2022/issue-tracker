b52842d5019eab9d139f05287a1c9d5fd404781c
banjjak2 <interr0bang@naver.com>
2022-06-20 21:56:11 +0900

    feat: Label 데이터클래스 수정 및 데이터바인딩 적용
    데이터클래스 변수 추가
    - 기존에는 데이터클래스에 id값이 없었으나, 조회 및 삭제 등 서버와 통신할 때 필요할 것으로 판단해 추가
    - 선택된 데이터인지 판별하기 위해 checked 변수 추가
    - 편집모드, 기본모드에 따라 다르게 뷰가 보여져야 하므로 mode 변수 추가
    - 프래그먼트간 데이터 송수신 시 클래스를 직렬화 해주어야 하므로 parcelable 인터페이스 상속 및 구현

--- app/src/main/java/com/example/it/issuetracker/domain/model/Label.kt
+++ app/src/main/java/com/example/it/issuetracker/domain/model/Label.kt
@@ -1,8 +1,53 @@
 package com.example.it.issuetracker.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
+import com.example.it.issuetracker.data.dto.LabelDto
+import com.example.it.issuetracker.presentation.main.issue.Mode
+
 data class Label(
-    val name: String,
+    val id: Int,
+    val title: String,
     val description: String = "",
     val color: String,
     val textColor: String,
-)
+    var isChecked: Boolean = false,
+    val mode: Mode = Mode.DEFAULT
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(color)
+        parcel.writeString(textColor)
+        parcel.writeByte(if (isChecked) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Label> {
+        override fun createFromParcel(parcel: Parcel): Label {
+            return Label(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Label?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+fun Label.toDto(): LabelDto {
+    return LabelDto(id, title, description, color, textColor)
+}
