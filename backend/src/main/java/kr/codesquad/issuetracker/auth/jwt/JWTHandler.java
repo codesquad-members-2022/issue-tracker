8780e19192e500377b080aae161edabc2f43a80c
Jaehong Choe <qnalsfj@gmail.com>
2022-06-20 16:54:49 +0900

    feat: JWTUtil클래스 리뷰 반영
    - util성 클래스이므로 인스턴스화를 막기 위해 private 생성자를 추가.

--- backend/src/main/java/kr/codesquad/issuetracker/auth/jwt/JWTHandler.java
+++ backend/src/main/java/kr/codesquad/issuetracker/auth/jwt/JWTHandler.java
@@ -6,13 +6,18 @@ import java.nio.charset.StandardCharsets;
 import javax.crypto.SecretKey;
 import kr.codesquad.issuetracker.auth.GitHubUserInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class JWTUtil {
+public class JWTHandler {
 
     private final JwtProperties jwtProperties;
+
+    private JWTHandler() throws InstantiationException {
+        throw new InstantiationException();
+    }
 
     public JWT createToken(GitHubUserInfo gitHubUserInfo) {
         String jwt = Jwts.builder()
