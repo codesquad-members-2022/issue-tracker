23289ef952ffdc12c5d113765c15f8e12eb7ad8c
Sol <hskim6521@naver.com>
2022-06-20 11:58:15 +0900

    refactor: UIColor의 너무 긴 이니셜라이저의 길이를 줄이기 위한 static 메소드들 선언 (bugpigg/issue-tracker#16)

--- iOS/IssueTracker/IssueTracker/Utility/Extension/UIColor+Extension.swift
+++ iOS/IssueTracker/IssueTracker/Utility/Extension/UIColor+Extension.swift
@@ -9,24 +9,53 @@ import UIKit
 
 extension UIColor {
     convenience init(hex: String, alpha: CGFloat = 1.0) {
-        var hexFormat: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-
-        if hexFormat.hasPrefix("#") {
-            hexFormat = String(hexFormat.dropFirst())
-        }
+        let hexFormat = Self.setHexFormat(hex)
 
         if hexFormat.count != 6 {
             self.init(red: 1, green: 1, blue: 1, alpha: alpha)
             return
         }
 
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexFormat).scanHexInt64(&rgbValue)
+        let rgbValue = Self.scanHex(hexFormat)
 
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat((rgbValue & 0x0000FF)) / 255.0,
+        self.init(red: Self.color(from: rgbValue, colour: .red),
+                  green: Self.color(from: rgbValue, colour: .blue),
+                  blue: Self.color(from: rgbValue, colour: .green),
                   alpha: alpha)
         return
+    }
+}
+
+fileprivate extension UIColor {
+    static func setHexFormat(_ hex: String) -> String {
+        var hexForamt = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexForamt.hasPrefix("#") {
+            hexForamt = String(hexForamt.dropFirst())
+        }
+
+        return hexForamt
+    }
+
+    static func scanHex(_ hex: String) -> UInt64 {
+        var rgbValue: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&rgbValue)
+
+        return rgbValue
+    }
+
+    static func color(from rgbValue: UInt64, colour: Colour) -> CGFloat {
+        switch colour {
+        case .red:
+            return CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        case .green:
+            return CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        case .blue:
+            return CGFloat((rgbValue & 0x0000FF)) / 255.0
+        }
+    }
+
+    enum Colour {
+        case red, green, blue
     }
 }
