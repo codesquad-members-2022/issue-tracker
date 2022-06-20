03f633a95d4b6bf7053efc763777a6ce976c1b52
otterp012 <otterp012@gmail.com>
2022-06-20 18:26:14 +0900

    [chore]: jest 설치 ak2j38/issue-tracker#23

---
+++ FE/config/jest.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      { configFile: './config/babel.conf.js' },
+    ],
+  },
+  roots: ['..'],
+  testEnvironment: 'jest-environment-jsdom',
+};
