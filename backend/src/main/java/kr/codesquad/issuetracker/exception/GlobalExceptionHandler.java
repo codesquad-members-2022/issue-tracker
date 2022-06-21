7571e1fa586984e2350fba5b0f95fdc60ea8b987
Jaehong Choe <qnalsfj@gmail.com>
2022-06-22 02:05:08 +0900

    feat: 글로벌 예외처리를 위한 핸들러 및 예외응답 클래스 추가

---
+++ backend/src/main/java/kr/codesquad/issuetracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package kr.codesquad.issuetracker.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+    }
+
+}
