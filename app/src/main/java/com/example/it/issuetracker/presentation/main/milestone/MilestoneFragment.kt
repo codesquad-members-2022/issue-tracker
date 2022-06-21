af973de21807b39e47b0121dc0b4b5f88f10961c
banjjak2 <interr0bang@naver.com>
2022-06-21 17:58:45 +0900

    feat: 마일스톤 UI 디자인 구현

--- app/src/main/java/com/example/it/issuetracker/presentation/main/milestone/MilestoneFragment.kt
+++ app/src/main/java/com/example/it/issuetracker/presentation/main/milestone/MilestoneFragment.kt
@@ -1,19 +1,25 @@
 package com.example.it.issuetracker.presentation.main.milestone
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.it.issuetracker.R
+import com.example.it.issuetracker.databinding.FragmentMilestoneBinding
+import com.example.it.issuetracker.presentation.main.label.LabelListAdapter
 
 class MilestoneFragment : Fragment() {
+
+    private lateinit var binding: FragmentMilestoneBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_milestone, container, false)
+    ): View {
+        binding = FragmentMilestoneBinding.inflate(inflater)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        return binding.root
     }
 }
