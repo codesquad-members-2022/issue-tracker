8684136c7e345d91354db480c85993cf210b4102
Hemudi <ksum1205@naver.com>
2022-06-21 21:16:43 +0900

    Refactor: Tabs 컴포넌트에 Icon 컴포넌트 추가
    jindonyy/issue-tracker/#19

--- FE/src/components/Tabs/index.tsx
+++ FE/src/components/Tabs/index.tsx
@@ -3,7 +3,7 @@ import { COLOR } from '@/styles/common';
 import { $Tab, $TabItem } from '@/components/Tabs/style';
 import { listItem, ITabs } from '@/components/Tabs/type';
 import { IButtonProps, IButtonStyleProps } from '@/components/Button/type';
-import { IconTypes } from '@/components/Icon';
+import { Icon } from '@/components/Icon';
 
 const hoverStyle: IButtonStyleProps = {
   background: COLOR.inputBackground,
@@ -15,15 +15,13 @@ const activeStyle: IButtonStyleProps = {
   color: COLOR.body
 };
 
-const getButtonProps = (iconType?: IconTypes): IButtonProps => {
-  return {
-    styleType: 'mediumText',
-    iconType: iconType,
-    width: '100%',
-    height: '100%',
-    hoverStyle: hoverStyle,
-    activeStyle: activeStyle
-  };
+const buttonPropsValue: IButtonProps = {
+  styleType: 'mediumText',
+  width: '100%',
+  height: '100%',
+  gap: '8px',
+  hoverStyle: hoverStyle,
+  activeStyle: activeStyle
 };
 
 const createButtonText = ({ name, count }: listItem) => `
@@ -35,7 +33,10 @@ export default function Tabs({ list }: ITabs) {
     <$Tab>
       {list?.map(({ name, iconType, count }) => (
         <$TabItem key={name}>
-          <Button {...getButtonProps(iconType)}>{createButtonText({ name, count })}</Button>
+          <Button {...buttonPropsValue}>
+            {iconType && <Icon IconType={iconType} />}
+            {createButtonText({ name, count })}
+          </Button>
         </$TabItem>
       ))}
     </$Tab>
