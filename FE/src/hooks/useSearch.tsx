1c2fb7222750f405fce37958b865fe2232c36f66
Cola <bcad1591@gmail.com>
2022-06-21 15:51:19 +0900

    [FE][Cola] useSearch 리팩토링 및 파라미터 사용방법 추가
    * feat: #64 useSearch 기능 추가
    
    init 함수 추가
    - 서치 파라미터의 값을 없애고 전달된 인자로 세팅
    - 인자는 key, value로 전달하거나, { paramValue: string }으로 전달
    
    * refactor: #64 add 함수 파라미터 종류 추가
    
    - add(key, value)로 전달하거나, add({ paramValue: string })로 전달
    
    * refactor: #64 replace 함수 파라미터 종류 추가
    
    - add(key, value)로 전달하거나, add({ paramValue: string })로 전달

--- FE/src/hooks/useSearch.tsx
+++ FE/src/hooks/useSearch.tsx
@@ -8,8 +8,14 @@ export const useSearch = (
   const [searchParams, setSearchParams] = useSearchParams();
   const paramValue = searchParams.get(paramKey) ?? defaultValue;
 
-  const add = (key: string, value: string) => {
+  const add = (param1: string | { paramValue: string }, param2?: string) => {
+    const [key, value] =
+      typeof param1 === 'string'
+        ? [param1, param2]
+        : param1.paramValue.split(sep);
+
     const targetString = [key, value].join(sep);
+
     if (paramValue.includes(targetString)) {
       return;
     }
@@ -19,8 +25,20 @@ export const useSearch = (
     setSearchParams(newSearchParams);
   };
 
-  const replace = (key: string, value: string) => {
+  const replace = (
+    param1: string | { paramValue: string },
+    param2?: string,
+  ) => {
+    const [key, value] =
+      typeof param1 === 'string'
+        ? [param1, param2]
+        : param1.paramValue.split(sep);
+
     const targetString = [key, value].join(sep);
+
+    // 예시) sep 가 :라면
+    // key:word 인 모든 값들에 매칭됨. (띄어쓰기는 포함하지 않음)
+    // "?q=key1:value1 key2:value2"인 경우 "key1:value1"와 "key2:value2"에 매칭됨.
     const regex = new RegExp(`${key}${sep}\\w*`, 'g');
 
     if (paramValue.includes(targetString)) {
@@ -37,5 +55,16 @@ export const useSearch = (
     setSearchParams(newSearchParams);
   };
 
-  return { paramValue, add, replace };
+  const init = (param1: string | { paramValue: string }, param2?: string) => {
+    if (typeof param1 === 'string') {
+      const [key, value] = [param1, param2];
+
+      setSearchParams(`${paramKey}=${key}${sep}${value}`);
+      return;
+    }
+
+    setSearchParams(`${paramKey}=${param1.paramValue}`);
+  };
+
+  return { paramValue, add, replace, init };
 };
