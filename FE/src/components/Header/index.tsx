cc2f97b6ea6f294bbefb08a80c68bfc8ba08030c
Hemudi <ksum1205@naver.com>
2022-06-22 16:46:12 +0900

    Style: Header 컴포넌트 import 경로 수정

--- FE/src/components/Header/index.tsx
+++ FE/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
+import Logo from '@/components/Logo';
+import UserProfile from '@/components//UserProfile';
 import { $Header } from '@/components/Header/style';
-import Logo from '../Logo';
-import UserProfile from '../UserProfile';
-import { IUserProfile } from '../UserProfile/type';
+import { IUserProfile } from '@/components//UserProfile/type';
 
 export default function Header({ src }: IUserProfile) {
   return (
