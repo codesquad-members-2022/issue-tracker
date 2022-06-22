5c0a85f87a974737a149f317c9a3576524f38c0c
soralee <sora2821@gmail.com>
2022-06-22 17:50:35 +0900

    Feat:  현재시각 ISOStrigng과 작성시각의 차이를 계산하는 util 함수 구현

--- FE/issuetracker/src/utils/util.ts
+++ FE/issuetracker/src/utils/util.ts
@@ -1,3 +1,17 @@
 export function keyMaker() {
   return Math.random().toString(36).substring(2, 11);
 }
+
+export function getNowISOString() {
+  const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  const currentDate = new Date(Date.now() - timezoneOffset);
+  const currentDateISOString = currentDate.toISOString().slice(0, -5);
+  return currentDateISOString;
+}
+
+export function calculateInterval(writeTime: string) {
+  const before = new Date(writeTime).getTime();
+  const now = new Date().getTime();
+  const interval = Math.floor((now - before) / 1000);
+  return interval;
+}
