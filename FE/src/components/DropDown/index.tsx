4bb382effd4c01341c1a97f4d56526fa93ed4237
jindonyy <rsefa94@gmail.com>
2022-06-22 16:35:17 +0900

    Feat: Dropdown 컴포넌트 구현
    jindonyy/issue-tracker/#17

--- FE/src/components/DropDown/index.tsx
+++ FE/src/components/DropDown/index.tsx
@@ -1,18 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Button from '@/components/Button';
 import { Icon } from '@/components/Icon';
+import Panel from '@/components/Dropdown/Panel';
+import { IDropDown } from '@/components/Dropdown/type';
+import { $DropDown } from '@/components/Dropdown/style';
 
-interface IDropDown {
-  title: string;
-}
-
-export default function DropDown({ title }: IDropDown) {
+export default function DropDown({ indicatorName, panelName, ...panelProps }: IDropDown) {
   const [isOpen, setIsOpen] = useState(false);
+  const panelRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    panelRef?.current?.focus();
+  }, [isOpen]);
 
   return (
-    <Button styleType="mediumText" onClick={() => setIsOpen(!isOpen)}>
-      {title}
-      {isOpen ? <Icon IconType="arrowUp" /> : <Icon IconType="arrowDown" />}
-    </Button>
+    <$DropDown>
+      <Button styleType="mediumText" onClick={() => setIsOpen(!isOpen)}>
+        {indicatorName}
+        <Icon iconType={isOpen ? 'arrowUp' : 'arrowDown'} />
+      </Button>
+      {isOpen && (
+        <Panel
+          {...panelProps}
+          panelRef={panelRef}
+          title={panelName}
+          handlePanelBlur={() => setIsOpen(!isOpen)}
+        />
+      )}
+    </$DropDown>
   );
 }
