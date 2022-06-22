03ad977655a766c8b1c291582552fd25cd8f11e3
soralee <sora2821@gmail.com>
2022-06-22 12:03:53 +0900

    feat: issueTitleInput 구현

---
+++ FE/issuetracker/src/components/Issue/IssueTitleInput.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import * as S from 'components/Issue/styled.issueTitleInput';
+
+interface Props {
+  inputValue: string;
+  setInputValue: (inputValue: string) => void;
+}
+
+function IssueTitleInput({ inputValue, setInputValue }: Props) {
+  const [isInputFocused, setInputFocus] = useState(false);
+
+  const onFocus = () => {
+    setInputFocus(true);
+  };
+
+  const onBlur = () => {
+    setInputFocus(false);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <S.titleInputWrapper isInputFocused={isInputFocused} onFocus={onFocus} onBlur={onBlur}>
+      <S.titleInputLabel>제목</S.titleInputLabel>
+      <S.titleInput value={inputValue} onChange={onChange} />
+    </S.titleInputWrapper>
+  );
+}
+export default IssueTitleInput;
