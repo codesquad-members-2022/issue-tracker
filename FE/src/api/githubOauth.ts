cbca1b8cb9fe8b92b4b1c21b7523fb111991195e
Hemudi <ksum1205@naver.com>
2022-06-23 01:22:19 +0900

    Feat: axios 를 이용하여 api 함수 구현
    jindonyy/issue-tracker/#21
    
    Co-authored-by: Dony <rsefa94@gmail.com>

---
+++ FE/src/api/githubOauth.ts
@@ -0,0 +1,15 @@
+import { requestApi } from '@/api/core';
+const getLoginURL = async () =>
+  await requestApi({
+    method: 'get',
+    url: '/login/oauth/github'
+  });
+
+const getLoginToken = async () =>
+  await requestApi({
+    method: 'get',
+    url: '/login/oauth/github/callback?code=12345678'
+    // config: { withCredentials: true }  //-> 쿠키를 추가해주기 위한 옵션(현재 CORS ERROR 발생)
+  });
+
+export { getLoginURL, getLoginToken };
