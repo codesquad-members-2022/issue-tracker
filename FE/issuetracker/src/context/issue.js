2085b8468b91467982d474d6686ee1529a31b994
soralee <sora2821@gmail.com>
2022-06-23 00:51:55 +0900

    refactor: issueState의 milestone, labels 값 수정

--- FE/issuetracker/src/context/issue.js
+++ FE/issuetracker/src/context/issue.js
@@ -33,13 +33,24 @@ const initialData = {
       description: '마감일은 언제인가요??',
     },
   ],
-  milestone: '',
+  milestone: {
+    id: 1,
+    title: '이슈트래커 1주차',
+    description: '개발 1주차',
+    dueDate: '2022-06-19',
+    progress: 0.6666666666666666,
+    openedIssue: 2,
+    closedIssue: 1,
+  },
   labels: [
     {
       id: 1,
       title: 'documentation',
       description: '서비스에 대한 개선 사항 혹은 추가 사항',
-      color: 'blue2',
+      color: {
+        font: 'gray1',
+        background: 'blue2',
+      },
     },
   ],
   assignees: [
