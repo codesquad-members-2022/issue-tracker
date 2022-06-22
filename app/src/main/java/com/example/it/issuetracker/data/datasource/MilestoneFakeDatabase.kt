634a6da94853f4c66d2d19fe8e8f6562789fcb78
banjjak2 <interr0bang@naver.com>
2022-06-22 12:44:21 +0900

    feat: 마일스톤 임시 메모리 데이터베이스 구현

---
+++ app/src/main/java/com/example/it/issuetracker/data/datasource/MilestoneFakeDatabase.kt
@@ -0,0 +1,59 @@
+package com.example.it.issuetracker.data.datasource
+
+import com.example.it.issuetracker.data.dto.MilestoneDto
+import kotlinx.coroutines.delay
+
+object MilestoneFakeDatabase {
+
+    val database = mutableListOf(
+        MilestoneDto(
+            id = 1,
+            title = "마일스톤",
+            deadLine = "2022-06-14",
+            description = ""
+        ),
+        MilestoneDto(
+            id = 2,
+            title = "마스터즈 코스",
+            deadLine = "2022-06-16",
+            description = ""
+        ),
+        MilestoneDto(
+            id = 3,
+            title = "테스트 그룹",
+            deadLine = "2022-06-17",
+            description = ""
+        ),
+        MilestoneDto(
+            id = 4,
+            title = "마스터즈 코스 숫자",
+            deadLine = "2022-06-20",
+            description = ""
+        )
+    )
+
+    val size: Int
+        get() = database.size
+
+    suspend fun add(milestoneDto: MilestoneDto) {
+        delay(1000)
+        database.add(milestoneDto)
+    }
+
+    suspend fun getAll(): List<MilestoneDto> {
+        delay(1000)
+        return database.toList()
+    }
+
+    suspend fun edit(milestoneDto: MilestoneDto) {
+        delay(1000)
+        val data = database.find { it.id == milestoneDto.id }
+        val index = database.indexOf(data)
+        database[index] = milestoneDto
+    }
+
+    suspend fun delete(id: Long) {
+        val milestoneInfo = database.find { it.id == id }
+        database.remove(milestoneInfo)
+    }
+}
