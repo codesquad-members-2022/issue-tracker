f898fd486fa53a9f87ee853ca5303cd6c3ed276b
Sangbeom Heo <mansaout@gmail.com>
2022-06-22 10:12:46 +0900

    Design: 필터바 ui 완성

--- FE/src/common/FilterBar.tsx
+++ FE/src/common/FilterBar.tsx
@@ -1,30 +1,81 @@
+import React, { useRef, useState } from 'react';
+import Icon from '@/assets/icons/Icon';
 import { GREYSCALE } from '@/constants';
-import React from 'react';
 import styled from 'styled-components';
 
 type FilterBarProps = {};
 
 function FilterBar({}: FilterBarProps) {
+  const [value, setValue] = useState('is:issue is:open');
+  const [isFocus, setFocus] = useState(false);
+  const inputRef = useRef(null);
+
+  const iconColor = isFocus ? GREYSCALE.LABEL : GREYSCALE.PLACEHOLDER;
+  const bgColor = isFocus ? GREYSCALE.OFF_WHITE : GREYSCALE.INPUT_BACKGROUND;
+
+  const onChange = (event) => setValue(event.target.value);
+
   return (
     <FilterBarBox>
-      <div>필터</div>
-      <input type="text" placeholder="isopen" />
+      <ButtonBox>
+        필터 <Icon iconName="arrowBottom" stroke={iconColor} />
+      </ButtonBox>
+      <InputBox>
+        <Icon iconName="search" stroke={iconColor} />
+        <Input
+          type="text"
+          placeholder="Search all issues"
+          value={value}
+          onChange={onChange}
+          ref={inputRef}
+        />
+      </InputBox>
     </FilterBarBox>
   );
 }
 
 const FilterBarBox = styled.div`
-  ${({ theme }) => theme.LAYOUT.flexLayoutMixin('row')}
+  ${({ theme }) => theme.LAYOUT.flexLayoutMixin('row', 'center', 'center')}
+  width:600px;
+  border-radius: 12px;
+  overflow: hidden;
+  background-color: ${GREYSCALE.INPUT_BACKGROUND};
+  border: 1px solid ${GREYSCALE.LINE};
 `;
 
-const InputBox = styled.input`
-  width: 340px;
-  height: 64px;
+const ButtonBox = styled.button`
+  ${({ theme }) =>
+    theme.LAYOUT.flexLayoutMixin('row', 'space-between', 'center')}
+  padding:6px 24px;
+  width: 128px;
+  height: 40px;
+  color: ${GREYSCALE.LABEL};
+  background-color: ${GREYSCALE.BACKGROUND};
+  border-right: 1px solid ${GREYSCALE.LINE};
+  ${({ theme }) => theme.TYPOGRAPHY.LINK_SMALL};
+
+  &:hover {
+    color: ${GREYSCALE.BODY};
+    background-color: ${GREYSCALE.LINE};
+  }
+`;
+
+const InputBox = styled.div`
+  position: relative;
+
+  svg {
+    position: absolute;
+    top: 12px;
+    left: 24px;
+  }
+`;
+
+const Input = styled.input`
+  width: 472px;
+  height: 40px;
+  padding: 6px 24px 6px 48px;
   background-color: ${GREYSCALE.INPUT_BACKGROUND};
-  border-radius: 16px;
-  padding: 18px 24px;
   color: ${GREYSCALE.TITLE_ACTION};
-  border: 1px solid ${GREYSCALE.INPUT_BACKGROUND};
   ${({ theme }) => theme.TYPOGRAPHY.TEXT_SMALL}
 
   ::placeholder {
@@ -33,7 +84,6 @@ const InputBox = styled.input`
 
   :focus {
     background-color: ${GREYSCALE.OFF_WHITE};
-    border: 1px solid ${GREYSCALE.TITLE_ACTION};
   }
 `;
 
--- FE/src/issueList/IssueListPage.tsx
+++ FE/src/issueList/IssueListPage.tsx
@@ -2,19 +2,27 @@ import React from 'react';
 import FilterBar from '@/common/FilterBar';
 import Taps from '@/common/Taps';
 import ButtonSmallStandard from '@/common/ButtonSmallStandard';
+import styled from 'styled-components';
 
 function IssueListPage() {
   return (
     <div>
-      <FilterBar />
-      <Taps />
-      <ButtonSmallStandard
-        isDisabled={false}
-        label="이슈 작성"
-        onClick={() => console.log('click 이슈작성버튼')}
-      />
+      <Wrap>
+        <FilterBar />
+        <Taps />
+        <ButtonSmallStandard
+          isDisabled={false}
+          label="이슈 작성"
+          onClick={() => console.log('click 이슈작성버튼')}
+        />
+      </Wrap>
     </div>
   );
 }
+
+const Wrap = styled.div`
+  ${({ theme }) => theme.LAYOUT.flexLayoutMixin('row')}
+  width:1280px;
+`;
 
 export default IssueListPage;
