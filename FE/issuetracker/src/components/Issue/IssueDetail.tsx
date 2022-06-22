7cdcd7f654f2c28f89fa1c2416a70f1616e98a50
soralee <sora2821@gmail.com>
2022-06-22 12:09:55 +0900

    refactor: IssueDetail에서 IssueLabel 코드를 분리하고, issueState atom 적용

--- FE/issuetracker/src/components/Issue/IssueDetail.tsx
+++ FE/issuetracker/src/components/Issue/IssueDetail.tsx
@@ -1,13 +1,13 @@
-import * as I from 'design/icons';
+import { useRecoilValue } from 'recoil';
 import * as S from 'components/Issue/styled.issueDetail';
+import IssueLabel from 'components/Issue/IssueLabel';
+import { issueState } from 'recoil/atoms/issue';
 
 function IssueDetail() {
+  const issueData = useRecoilValue(issueState);
   return (
     <S.issueDetailWrapper>
-      <S.issueLabel>
-        <I.alertCircle />
-        <S.labelText>열린 이슈</S.labelText>
-      </S.issueLabel>
+      <IssueLabel isIssueClosed={issueData.closed} />
       <S.issueDetail>이 이슈가 20분 전에 Oni님에 의해 열렸습니다 - 코멘트 1개</S.issueDetail>
     </S.issueDetailWrapper>
   );
--- FE/issuetracker/src/components/Issue/styled.issueDetail.js
+++ FE/issuetracker/src/components/Issue/styled.issueDetail.js
@@ -6,21 +6,6 @@ export const issueDetailWrapper = styled.div`
   margin: 16px 0;
 `;
 
-export const issueLabel = styled.label`
-  ${mixin.flexbox({ horizontal: 'center', vertical: 'center' })};
-  width: 100px;
-  height: 40px;
-  border: 2px solid ${({ theme }) => theme.backgroundColors.blue2};
-  border-radius: 30px;
-  margin-right: 8px;
-  background: ${({ theme }) => theme.backgroundColors.blue1};
-  color: ${({ theme }) => theme.fontColors.blue};
-`;
-
-export const labelText = styled.span`
-  ${({ theme }) => theme.fontStyles.textXSmall};
-`;
-
 export const issueDetail = styled.span`
   ${({ theme }) => theme.fontStyles.textMedium};
   color: ${({ theme }) => theme.fontColors.gray3};
