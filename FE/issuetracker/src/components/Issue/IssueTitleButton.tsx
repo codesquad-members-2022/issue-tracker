216eff5fe2a101e2536bfee83e4c2cbaf8dd808e
soralee <sora2821@gmail.com>
2022-06-22 12:05:26 +0900

    refactor: IssueTitleButton 수정

--- FE/issuetracker/src/components/Issue/IssueTitleButton.tsx
+++ FE/issuetracker/src/components/Issue/IssueTitleButton.tsx
@@ -3,11 +3,13 @@ import * as S from 'components/Issue/styled.issueTitleButton';
 interface Props {
   buttonIcon: React.ReactNode;
   buttonText: string;
+  buttonState: string;
+  clickHandler: () => void;
 }
 
-function IssueTitleButton({ buttonIcon, buttonText }: Props) {
+function IssueTitleButton({ buttonIcon, buttonText, buttonState, clickHandler }: Props) {
   return (
-    <S.issueButton>
+    <S.issueButton buttonState={buttonState} onClick={clickHandler}>
       {buttonIcon}
       <S.buttonText>{buttonText}</S.buttonText>
     </S.issueButton>
--- FE/issuetracker/src/components/Issue/styled.issueTitleButton.ts
+++ FE/issuetracker/src/components/Issue/styled.issueTitleButton.ts
@@ -1,15 +1,29 @@
 import styled from 'styled-components';
 import { mixin } from 'design/GlobalStyles';
 
-export const issueButton = styled.button.attrs(() => ({ type: 'button' }))`
+export const issueButton = styled.button.attrs(() => ({ type: 'button' }))<{
+  buttonState: string;
+}>`
   ${mixin.flexbox({ horizontal: 'center', vertical: 'center' })};
   width: 120px;
   height: 40px;
-  border: 2px solid ${({ theme }) => theme.backgroundColors.blue2};
   border-radius: 11px;
   margin-left: 8px;
-  background: ${({ theme }) => theme.backgroundColors.gray1};
-  color: ${({ theme }) => theme.fontColors.blue};
+  ${({ buttonState, theme }) => {
+    switch (buttonState) {
+      case 'active':
+        return `
+        background: ${theme.backgroundColors.blue2};
+        color: ${theme.fontColors.gray1};
+        `;
+      default:
+        return `
+        border: 2px solid ${theme.backgroundColors.blue2};
+        background: ${theme.backgroundColors.gray1};
+        color: ${theme.fontColors.blue};
+        `;
+    }
+  }};
 `;
 
 export const buttonText = styled.span`
