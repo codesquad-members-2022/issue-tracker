491c95ba856bd6876991989b8418175a3855c284
sanghee <dev.sanghee@gmail.com>
2022-06-22 23:35:58 +0900

    Chore: 불필요한 코드 제거

--- PRTracker/Source/Login/View/LoginViewController.swift
+++ PRTracker/Source/Login/View/LoginViewController.swift
@@ -13,10 +13,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var githubLoginButton: UIButton!
     
     @IBAction func emaliLoginButtonTapped(_ sender: Any) {
-        // 임시적으로 이메일 로그인 버튼 클릭 시 메인 화면으로 이동하도록 구현
-        DispatchQueue.main.async {
-            self.performSegue(withIdentifier: "mainSegue", sender: nil)
-        }
     }
     
     @IBAction func githubLoginButtonTapped(_ sender: Any) {
