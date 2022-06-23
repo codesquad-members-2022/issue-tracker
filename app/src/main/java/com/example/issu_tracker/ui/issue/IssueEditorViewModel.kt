7b569c3c490b382569521db94e277d14522a8124
Linus <zerolecture0922@gmail.com>
2022-06-24 02:10:19 +0900

    feat : 이슈 작성 저장 누르면 새 이슈를 firebase 에 생성하기
    - 이슈 제목, 본문(코멘트), 조건들의 값을 ViewModel에 전달
    - 전달한 String 값과 일치하는 조건 데이터 클래스들을 저장
    - 저장한 데이터 클래스, 이슈 제목 및 본문을 바탕으로 Issue 데이터 생성하여 Repository 에 전달
    - Repo 에서 전달받은 값을 firebase 데이터 베이스에 저장

--- app/src/main/java/com/example/issu_tracker/ui/issue/IssueEditorViewModel.kt
+++ app/src/main/java/com/example/issu_tracker/ui/issue/IssueEditorViewModel.kt
@@ -2,6 +2,9 @@ package com.example.issu_tracker.ui.issue
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.issu_tracker.data.Issue
+import com.example.issu_tracker.data.Label
+import com.example.issu_tracker.data.User
 import com.example.issu_tracker.data.repository.IssueEditorRepository
 import com.example.issu_tracker.ui.common.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,9 +23,11 @@ class IssueEditorViewModel @Inject constructor(val repository: IssueEditorReposi
     private val _issueBodyStateFlow = MutableStateFlow<String>("")
     val issueBodyStateFlow = _issueBodyStateFlow.asStateFlow()
 
+    private val _assigneeList = mutableListOf<User>()
     private val _assigneeStateFlow = MutableStateFlow<List<String>>(mutableListOf(""))
     val assigneeStateFlow = _assigneeStateFlow.asStateFlow()
 
+    private val _labelList = mutableListOf<Label>()
     private val _labelStateFlow = MutableStateFlow<List<String>>(mutableListOf(""))
     val labelStateFlow = _labelStateFlow.asStateFlow()
 
@@ -30,10 +35,10 @@ class IssueEditorViewModel @Inject constructor(val repository: IssueEditorReposi
     val mileStoneStateFlow = _mileStoneStateFlow.asStateFlow()
 
     private val _imageUrlFromFireBaseStateFlow = MutableStateFlow<String>("")
-    val imageUrlFromFireBaseStateFlow  = _imageUrlFromFireBaseStateFlow.asStateFlow()
+    val imageUrlFromFireBaseStateFlow = _imageUrlFromFireBaseStateFlow.asStateFlow()
 
-    private var assigneeConditionValue = ""
-    private var labelConditionValue = ""
+    private var assigneeConditionValue: User = User()
+    private var labelConditionValue: Label = Label()
     private var mileStoneConditionValue = ""
 
     init {
@@ -51,35 +56,60 @@ class IssueEditorViewModel @Inject constructor(val repository: IssueEditorReposi
         _issueBodyStateFlow.value = text
     }
 
-    /*fun createIssue() {
-        val newIssue = NewIssue(
+    fun createIssue() {
+        val newIssue = Issue(
             id = "",
-        comments = listOf(),
-        description = _issueBodyStateFlow.value,
-        label = listOf(labelConditionValue),
-        mileStone = ,
-        state = true,
-        title = _issueTitleStateFlow.value,
-        user =
+            comments = listOf(),
+            description = _issueBodyStateFlow.value,
+            label = listOf(labelConditionValue),
+            mileStone = "마일스톤",
+            state = true,
+            title = _issueTitleStateFlow.value,
+            user = assigneeConditionValue
         )
-    }*/
+        viewModelScope.launch {
+            repository.createNewIssue(newIssue)
+        }
+    }
 
     private suspend fun setLabelList() {
-        val labelList = repository.loadLabel().toMutableList()
+        _labelList.addAll(repository.loadLabel())
+        val labelList = mutableListOf<String>()
+        _labelList.forEach {
+            labelList.add(it.content)
+        }
         labelList.add("")
         _labelStateFlow.emit(labelList)
     }
 
     private suspend fun setAssigneeList() {
-        val assigneeList = repository.loadAssignee().toMutableList()
+        _assigneeList.addAll(repository.loadAssignee())
+        val assigneeList = mutableListOf<String>()
+        _assigneeList.forEach {
+            assigneeList.add(it.name)
+        }
         assigneeList.add("")
         _assigneeStateFlow.emit(assigneeList)
     }
 
-    fun inputSpinnerValue(text: String, conditionType: Int) {
+    fun inputSpinnerValue(condition: String, conditionType: Int) {
         when (conditionType) {
-            Constants.CONDITION_TYPE_ASSIGNEE -> assigneeConditionValue = text
-            Constants.CONDITION_TYPE_LABEL -> labelConditionValue = text
+            Constants.CONDITION_TYPE_ASSIGNEE -> {
+                _assigneeList.forEach {
+                    if (it.name == condition) {
+                        assigneeConditionValue = it
+                        return
+                    }
+                }
+            }
+            Constants.CONDITION_TYPE_LABEL -> {
+                _labelList.forEach {
+                    if (it.content == condition) {
+                        labelConditionValue = it
+                        return
+                    }
+                }
+            }
 //            Constants.CONDITION_TYPE_ASSIGNEE -> mileStoneConditionValue = text
         }
     }
