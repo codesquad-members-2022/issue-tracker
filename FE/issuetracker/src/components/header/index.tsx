2a01b0b33bee2e703df09c9c755a0b6087dec80f
soralee <sora2821@gmail.com>
2022-06-23 14:44:53 +0900

    refactor: header 필요없는 속성 제거

--- FE/issuetracker/src/components/header/index.tsx
+++ FE/issuetracker/src/components/header/index.tsx
@@ -2,9 +2,8 @@ import * as S from 'components/header/styled.index';
 import AccountSrc from 'assets/images/UserImageLarge.svg';
 
 function Header() {
-  const isLoginPage = window.location.href === `http://localhost:3000/`;
   return (
-    <S.HeaderWrap checkPage={isLoginPage}>
+    <S.HeaderWrap>
       <S.Title to="/issueList">Issue Tracker</S.Title>
       <S.AccountImg src={AccountSrc} />
     </S.HeaderWrap>
