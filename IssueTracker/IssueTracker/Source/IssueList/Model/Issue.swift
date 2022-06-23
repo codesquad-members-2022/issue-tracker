287563aae673c9980ce34c09d5956f576a6a2f06
Damagucci-Juice <jsr9874@gmail.com>
2022-06-23 13:57:10 +0900

    fix: State 열거형을 Issue 안에 중첩 타입으로 선언

--- IssueTracker/IssueTracker/Source/IssueList/Model/Issue.swift
+++ IssueTracker/IssueTracker/Source/IssueList/Model/Issue.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 struct Issue: Codable {
+    enum State: String, Codable {
+        case open
+        case written
+        case assign
+        case comment
+        case closed
+    }
+
     let title: String
 //    let description: String
 //    let milestone: Milestone?
