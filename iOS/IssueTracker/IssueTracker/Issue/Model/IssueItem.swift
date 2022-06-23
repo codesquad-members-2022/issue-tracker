8fdc713cec54974cb5bbc96325ecdcbfe58b4848
Jason <ggj00125@gmail.com>
2022-06-23 13:12:07 +0900

    refactor: issueItem CodingKey 작성

--- iOS/IssueTracker/IssueTracker/Issue/Model/IssueItem.swift
+++ iOS/IssueTracker/IssueTracker/Issue/Model/IssueItem.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 
-struct IssueItem {
+struct IssueItem: Codable {
     let id: Int
     let title, content: String
     let milestoneName: String
     let labels: [Label]
     
+    enum CodingKeys: String, CodingKeys {
+        case id
+        case title, content
+        case milestoneName = "milestoneTitle"
+        case label
+    }
 }
 
 struct Label {
     let title: String
     let backgroundColor: String
+    let isDarkMode: Bool
 }
