a43a97a38b1f4425590c6677fbbd80517172ef51
ikjo93 <auddlr100@naver.com>
2022-06-24 00:59:05 +0900

    refactor : 리프레쉬 토큰 쿠키 응답 로직 리팩토링

--- be/src/main/java/codesquad/issuetracker/controller/AuthController.java
+++ be/src/main/java/codesquad/issuetracker/controller/AuthController.java
@@ -8,11 +8,11 @@ import codesquad.issuetracker.jwt.Token;
 import codesquad.issuetracker.jwt.TokenManager;
 import codesquad.issuetracker.jwt.TokenUtils;
 import codesquad.issuetracker.service.OAuthService;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,17 +36,15 @@ public class AuthController {
         tokenManager.saveRefreshTokenByMemberId(String.valueOf(memberId), refreshToken.getToken());
 
         response.addHeader("access-token", accessToken.getToken());
-        response.addCookie(createCookieWithRefreshToken(refreshToken.getToken()));
+        response.setHeader("Set-Cookie", ResponseCookie
+            .from("refresh-token", refreshToken.getToken())
+            .path("/api")
+            .build()
+            .toString());
 
         return new ResponseMessage(HttpStatus.OK, "로그인이 정상적으로 처리되었습니다.");
     }
 
-    private Cookie createCookieWithRefreshToken(String refreshToken) {
-        Cookie cookie = new Cookie("refresh-token", refreshToken);
-        cookie.setPath("/api");
-        cookie.setHttpOnly(true);
-        return cookie;
-    }
 
     @PostMapping("/api/access-token/reissue")
     public ResponseMessage reissue(HttpServletRequest request, HttpServletResponse response) {
