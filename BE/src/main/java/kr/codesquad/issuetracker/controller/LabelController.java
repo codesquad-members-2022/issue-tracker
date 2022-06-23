e72f77b048caa40066297007a990edc38d9ef41d
leekm0310 <leekm310@gmail.com>
2022-06-23 14:53:55 +0900

    Feat: 레이블 삭제 기능 구현

--- BE/src/main/java/kr/codesquad/issuetracker/controller/LabelController.java
+++ BE/src/main/java/kr/codesquad/issuetracker/controller/LabelController.java
@@ -3,7 +3,12 @@ package kr.codesquad.issuetracker.controller;
 import kr.codesquad.issuetracker.dto.LabelResponse;
 import kr.codesquad.issuetracker.service.LabelService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +22,15 @@ public class LabelController {
     @GetMapping("/labels")
     public List<LabelResponse> getLabelList() {
         return labelService.getLabelList();
+    }
+
+    @DeleteMapping("/labels/{id}")
+    public ResponseEntity deleteLabel(@PathVariable Long id) {
+        try {
+            labelService.deleteLabel(id);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
