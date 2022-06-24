838e3a95b1eeb510ec097267290a37bda2b5dc6d
jindonyy <rsefa94@gmail.com>
2022-06-24 15:33:32 +0900

    Feat: mock데이터 추가
    jindonyy/issue-tracker/#14

--- FE/src/components/IssueList/ListItem/type.ts
+++ FE/src/components/IssueList/ListItem/type.ts
@@ -14,8 +14,33 @@ interface IListItem {
   status: 'OPEN' | 'CLOSE';
 }
 
+interface ILabel {
+  id: number;
+  name: string;
+  description: string;
+  color_code: string;
+}
+
+interface IMilestone {
+  id: number;
+  name: string;
+  description: string;
+  target_date: string;
+  open_issue: number;
+  closed_issue: number;
+}
+
+type mockDataIssueListKey = 'issueList';
+
+type mockDataIssueOptionKey = 'labelList' | 'milestoneList';
+
+type MockData = Record<
+  mockDataIssueListKey & mockDataIssueOptionKey,
+  IListItem[] | ILabel[] | IMilestone[]
+>;
+
 interface I$Text {
   size: 'large' | 'small';
 }
 
-export type { IListItem, I$Text };
+export type { IListItem, ILabel, IMilestone, mockDataIssueOptionKey, MockData, I$Text };
