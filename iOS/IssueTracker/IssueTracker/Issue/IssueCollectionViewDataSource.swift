449bf2d08710649ef284e0ca8dec76b88ec61a11
Jason <ggj00125@gmail.com>
2022-06-24 15:55:05 +0900

    refactor: IssueViewController에 ViewModel과 View를 Bind하는 메서드 구현

--- iOS/IssueTracker/IssueTracker/Issue/IssueCollectionViewDataSource.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueCollectionViewDataSource.swift
@@ -9,28 +9,18 @@ import UIKit
 
 class IssueCollectionViewDataSource: NSObject, UICollectionViewDataSource {
     
-//    var data: [IssueItem] = [
-//        .init(id: 1, title: "제목", content: "이슈에 대한 설명", milestoneName: "마일스톤 이름", labels: [
-//            .init(title: "레이블 이름", backgroundColor: "#677ae5")
-//        ]),
-//        .init(id: 2, title: "제목", content: "이슈에 대한 설명", milestoneName: "마일스톤 이름", labels: [
-//            .init(title: "Bug", backgroundColor: "#20bb87"),
-//            .init(title: "iOS", backgroundColor: "#20bb87")
-//        ]),
-//        .init(id: 3, title: "제목", content: "이슈에 대한 설명", milestoneName: "마일스톤 이름", labels: [
-//            .init(title: "Bug", backgroundColor: "#20bb87"),
-//            .init(title: "iOS", backgroundColor: "#20bb87")
-//        ])
-//    ]
-    var issueListViewModel = IssueListViewModel()
+    let issueViewModel: IssueViewModel
+    
+    init(issueViewModel: IssueViewModel) {
+        self.issueViewModel = issueViewModel
+    }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        data.count
-        return self.issueListViewModel.allIssueViewModel.count
+        return self.issueViewModel.cellCount
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -38,8 +28,8 @@ class IssueCollectionViewDataSource: NSObject, UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IssueCollectionViewCell.identifier, for: indexPath) as? IssueCollectionViewCell else {
             return UICollectionViewCell()
         }
-//        let issueEntity = data[indexPath.item]
-//        cell.configure(with: issueEntity)
+        let issueEntity = issueViewModel.list.value[indexPath.item]
+        cell.configure(with: issueEntity)
         
         return cell
     }
