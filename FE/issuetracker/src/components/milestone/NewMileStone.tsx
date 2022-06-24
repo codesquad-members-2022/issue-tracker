4ae59f605ce559452c9162d601973b0eead42bef
soralee <sora2821@gmail.com>
2022-06-24 18:49:38 +0900

    feat:  NewMileStone 컴포넌트 구현

---
+++ FE/issuetracker/src/components/milestone/NewMileStone.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import * as I from 'design/icons';
+import * as S from 'components/milestone/styled/styled.newMileStone';
+import MileStoneInput from 'components/milestone/MileStoneInput';
+import SaveButton from 'components/common/SaveButton';
+
+function NewMileStone() {
+  return (
+    <S.newMileStoneWrapper>
+      <S.Title>새로운 마일스톤 추가</S.Title>
+      <S.inputTopWrapper>
+        <MileStoneInput labelText="제목" placeholder="마일스톤 이름" />
+        <MileStoneInput labelText="완료일(선택)" placeholder="완료일(선택) ex. YYYY-MM-DD" />
+      </S.inputTopWrapper>
+      <MileStoneInput labelText="설명(선택)" placeholder="설명(선택)" />
+      <SaveButton buttonText="완료" margin="24px 0 0 0" clickHandler={() => {}} />
+    </S.newMileStoneWrapper>
+  );
+}
+export default NewMileStone;
---
+++ FE/issuetracker/src/components/milestone/styled/styled.newMileStone.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { mixin } from 'design/GlobalStyles';
+
+export const newMileStoneWrapper = styled.div`
+  width: 100%;
+  height: 288px;
+  padding: 32px;
+  border: 1px solid ${({ theme }) => theme.backgroundColors.gray4};
+  border-radius: 16px;
+  background: ${({ theme }) => theme.backgroundColors.gray1};
+`;
+
+export const Title = styled.div`
+  ${({ theme }) => theme.fontStyles.textLarge};
+  color: ${({ theme }) => theme.fontColors.black};
+  margin-bottom: 24px;
+`;
+
+export const inputTopWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 16px;
+  height: 40px;
+  margin-bottom: 16px;
+`;
+
+export const inputWrapper = styled.div`
+  height: 40px;
+  padding: 0 24px;
+  border-radius: 11px;
+  background: ${({ theme }) => theme.backgroundColors.gray3};
+`;
+
+export const input = styled.input.attrs(() => ({ type: 'text' }))`
+  ${({ theme }) => theme.fontStyles.textSmall};
+  height: 40px;
+  color: ${({ theme }) => theme.fontColors.gray5};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.fontColors.gray4};
+  }
+`;
+
+export const inputLabel = styled.label`
+  width: 80px;
+  height: 40px;
+  margin-right: 8px;
+`;
