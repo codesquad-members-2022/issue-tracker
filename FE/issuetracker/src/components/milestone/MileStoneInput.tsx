636d713c5001645687ba8e99b5882b0e0a1491b7
soralee <sora2821@gmail.com>
2022-06-25 18:57:36 +0900

    feat: input에 onChange 이벤트 등록, mileStoneState 업데이트 기능 구현

--- FE/issuetracker/src/components/milestone/MileStoneInput.tsx
+++ FE/issuetracker/src/components/milestone/MileStoneInput.tsx
@@ -1,15 +1,17 @@
 import * as S from 'components/milestone/styled/styled.mileStoneInput';
 
 interface Props {
+  inputName: string;
   labelText: string;
   placeholder: string;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function MileStoneInput({ labelText, placeholder }: Props) {
+function MileStoneInput({ inputName, labelText, placeholder, handleInputChange }: Props) {
   return (
     <S.inputWrapper>
       <S.inputLabel>{labelText}</S.inputLabel>
-      <S.input placeholder={placeholder} />
+      <S.input name={inputName} placeholder={placeholder} onChange={handleInputChange} />
     </S.inputWrapper>
   );
 }
