46145cb679d0073c95cbfa75f809ce5293ff189e
banjjak2 <interr0bang@naver.com>
2022-06-27 11:21:36 +0900

    refactor: Label 추가 관련 상태를 저장을 각각의 변수에서 UI State 로 변경
    상태 관련 변수들을 하나의 데이터클래스로 둠으로써 상태를 관리하기 편하다고 판단
      - 기존과 같이 변수로 처리했을 때는 상태의 개수가 많아질수록 변수들이 많아지게 되어 실수할 가능성이 있음

--- app/src/main/java/com/example/it/issuetracker/presentation/main/label/add/LabelAddFragment.kt
+++ app/src/main/java/com/example/it/issuetracker/presentation/main/label/add/LabelAddFragment.kt
@@ -9,7 +9,10 @@ import com.example.it.issuetracker.R
 import com.example.it.issuetracker.databinding.FragmentLabelAddBinding
 import com.example.it.issuetracker.domain.model.Label
 import com.example.it.issuetracker.presentation.common.BaseFragment
+import com.example.it.issuetracker.presentation.common.Constants
 import com.example.it.issuetracker.presentation.common.repeatOnLifecycleExtension
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LabelAddFragment : BaseFragment<FragmentLabelAddBinding>(R.layout.fragment_label_add) {
@@ -27,19 +30,6 @@ class LabelAddFragment : BaseFragment<FragmentLabelAddBinding>(R.layout.fragment
         binding.lifecycleOwner = viewLifecycleOwner
         editLabelInfo = arguments?.getSerializable("label") as? Label
         binding.viewModel = viewModel
-        binding.labelAppbarLayout.toolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.save_string -> {
-                    if (editLabelInfo == null) {
-                        addLabel()
-                    } else {
-                        editLabel()
-                    }
-                    true
-                }
-                else -> false
-            }
-        }
 
         initView()
         observerData()
@@ -50,7 +40,7 @@ class LabelAddFragment : BaseFragment<FragmentLabelAddBinding>(R.layout.fragment
             binding.editSubject.text.toString(),
             binding.editDescription.text.toString(),
             binding.editBackground.text.toString(),
-            viewModel.textColor.value
+            viewModel.uiState.value.textColor
         )
     }
 
@@ -60,9 +50,8 @@ class LabelAddFragment : BaseFragment<FragmentLabelAddBinding>(R.layout.fragment
                 title = binding.editSubject.text.toString(),
                 description = binding.editDescription.text.toString(),
                 color = binding.editBackground.text.toString(),
-                textColor = viewModel.textColor.value
+                textColor = viewModel.uiState.value.textColor
             )
-
             viewModel.editLabel(labelInfo)
         }
     }
@@ -74,31 +63,48 @@ class LabelAddFragment : BaseFragment<FragmentLabelAddBinding>(R.layout.fragment
             binding.toolbarTitle = resources.getString(R.string.label_edit_string)
             viewModel.setData(editLabelInfo!!)
         }
-
         val saveMenu = binding.labelAppbarLayout.toolbar.menu.findItem(R.id.save_string)
+        binding.labelAppbarLayout.toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.save_string -> {
+                    if (editLabelInfo == null) addLabel()
+                    else editLabel()
+                    true
+                }
+                else -> false
+            }
+        }
         binding.labelAppbarLayout.toolbar.setNavigationOnClickListener { popBackStack() }
         binding.editSubject.doAfterTextChanged { input ->
             val text = input.toString()
             saveMenu.isEnabled = text.isNotEmpty()
-            viewModel.title.value = text
+            viewModel.setTitle(text)
+        }
+        binding.editDescription.doAfterTextChanged { input ->
+            val text = input.toString()
+            viewModel.setDescription(text)
         }
     }
 
     override fun observerData() {
         repeatOnLifecycleExtension {
-            viewModel.completeSaveLabel.collect { complete ->
-                if (complete) {
-                    clickSaveListener?.invoke()
-                    popBackStack()
+            viewModel.uiState.map { it.completeTask }
+                .distinctUntilChanged()
+                .collect { isCompleted ->
+                    if (isCompleted) {
+                        clickSaveListener?.invoke()
+                        popBackStack()
+                    }
                 }
-            }
         }
-
         repeatOnLifecycleExtension {
-            viewModel.error.collect { msgId ->
-                val msg = getString(msgId)
-                Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
-            }
+            viewModel.uiState.map { it.errorMsgId }
+                .distinctUntilChanged()
+                .collect { msgId ->
+                    if (msgId == Constants.INIT_ERROR_MSG_ID) return@collect
+                    val msg = getString(msgId)
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
+                }
         }
     }
 
--- app/src/main/java/com/example/it/issuetracker/presentation/main/label/add/LabelAddViewModel.kt
+++ app/src/main/java/com/example/it/issuetracker/presentation/main/label/add/LabelAddViewModel.kt
@@ -7,30 +7,17 @@ import com.example.it.issuetracker.data.dto.AddLabelDto
 import com.example.it.issuetracker.domain.model.Label
 import com.example.it.issuetracker.domain.model.toLabelDto
 import com.example.it.issuetracker.domain.repository.LabelRepository
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class LabelAddViewModel(
     private val labelRepository: LabelRepository,
 ) : ViewModel() {
 
-    var title = MutableStateFlow("")
-
-    var description = MutableStateFlow("")
-
-    var backgroundColor = MutableStateFlow("#B71C1C")
-
-    var textColor = MutableStateFlow("#FFFFFFFF")
-
-    private var _completeSaveLabel = MutableStateFlow(false)
-    val completeSaveLabel = _completeSaveLabel.asStateFlow()
-
-    private val _error = MutableSharedFlow<Int>()
-    val error = _error.asSharedFlow()
-
+    private var _uiState = MutableStateFlow(NewLabelUiState())
+    val uiState = _uiState.asStateFlow()
     private val colorMap = mapOf(
         "#B71C1C" to "#FFFFFFFF",
         "#E65100" to "#FFFFFFFF",
@@ -50,39 +37,48 @@ class LabelAddViewModel(
     )
 
     fun addLabel(title: String, description: String, backgroundColor: String, textColor: String) {
-        _completeSaveLabel.value = false
-        this.title.value = title
-        this.description.value = description
-        this.backgroundColor.value = backgroundColor
-        this.textColor.value = textColor
-
+        _uiState.update {
+            it.copy(
+                completeTask = false,
+                title = title,
+                description = description,
+                color = backgroundColor,
+                textColor = textColor
+            )
+        }
         val addLabelDto = AddLabelDto(title, description, backgroundColor, textColor)
         viewModelScope.launch {
             val result = labelRepository.addLabel(addLabelDto)
-            if (result.isFailure) _error.emit(R.string.network_error)
-            else _completeSaveLabel.value = true
+            if (result.isFailure) _uiState.update { it.copy(errorMsgId = R.string.network_error) }
+            else _uiState.update { it.copy(completeTask = true) }
         }
     }
 
     fun editLabel(label: Label) {
         viewModelScope.launch {
             val result = labelRepository.editLabel(label.toLabelDto())
-            if (result.isFailure) _error.emit(R.string.network_error)
-            else _completeSaveLabel.value = true
+            if (result.isFailure) _uiState.update { it.copy(errorMsgId = R.string.network_error) }
+            else _uiState.update { it.copy(completeTask = true) }
         }
     }
 
     fun randomColor() {
         val color = colorMap.keys.random()
         val contentColor = colorMap[color]
-        backgroundColor.value = color
-        textColor.value = contentColor ?: "#FFFFFFFF"
+        _uiState.update { it.copy(color = color, textColor = contentColor ?: "#FFFFFFFF") }
     }
 
     fun setData(label: Label) {
-        this.title.value = label.title
-        this.description.value = label.description
-        this.backgroundColor.value = label.color
-        this.textColor.value = label.textColor
+        _uiState.update {
+            it.copy(
+                title = label.title,
+                description = label.description,
+                color = label.color,
+                textColor = label.textColor
+            )
+        }
     }
+
+    fun setTitle(title: String) = _uiState.update { it.copy(title = title) }
+    fun setDescription(description: String) = _uiState.update { it.copy(description = description) }
 }
