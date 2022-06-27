6b49372fc7a55afa3fcd70cd71d5f8f01c403cd9
seok <dhdustnr0134@naver.com>
2022-06-27 16:55:55 +0900

    init : Querydsl 환경 설정

--- BE/src/main/java/louie/hanse/issuetracker/repository/CommentRepository.java
+++ BE/src/main/java/louie/hanse/issuetracker/repository/CommentRepository.java
@@ -1,7 +1,9 @@
 package louie.hanse.issuetracker.repository;
 
 import louie.hanse.issuetracker.domain.Comment;
+import louie.hanse.issuetracker.domain.QMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }
