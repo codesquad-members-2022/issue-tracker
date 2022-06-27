5c77e9f65eed742e55bbcc84e1ce90d0cd07b184
BumgeunSong <isp1195@gmail.com>
2022-06-27 19:49:48 +0900

    Chore: NetworkService에서 사용하지 않는 메서드 제거

--- PRTracker/Source/Common/Networking/NetworkService.swift
+++ PRTracker/Source/Common/Networking/NetworkService.swift
@@ -20,7 +20,6 @@ struct NetworkManger: NetworkService {
     }
     
     func execute<T: APIRequestable>(_ request: T, completion: @escaping (Result<T.ModelType, NetworkError>) -> Void) {
-
         session.dataTask(with: request.request) { data, response, error in
             if let error = error {
                 return completion(.failure(.networkFailure(error: error)))
@@ -44,40 +43,5 @@ struct NetworkManger: NetworkService {
             
             completion(.success(decoded))
         }.resume()
-    }
-
-    func request<T: Codable>(_ request: URLRequest, method: HTTPMethod, mediaType: MediaType, then completion: @escaping (Result<T, NetworkError>) -> Void) {
-        let request = configure(request, method: method, mediaType: mediaType)
-
-        session.dataTask(with: request) { data, response, error in
-            if let error = error {
-                return completion(.failure(.networkFailure(error: error)))
-            }
-
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
-                return completion(.failure(.failedParsingHTTPResponse))
-            }
-
-            guard (200..<300).contains(statusCode) else {
-                return completion(.failure(.unexpectedStatusCode(statusCode)))
-            }
-
-            guard let data = data else {
-                return completion(.failure(.missingData))
-            }
-
-            guard let decoded = try? JSONDecoder().decode(T.self, from: data) else {
-                return completion(.failure(.failedDecoding(type: "\(T.self)")))
-            }
-
-            completion(.success(decoded))
-        }.resume()
-    }
-
-    private func configure(_ request: URLRequest, method: HTTPMethod, mediaType: MediaType) -> URLRequest {
-        var request = request
-        request.httpMethod = "\(method)"
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        return request
     }
 }
