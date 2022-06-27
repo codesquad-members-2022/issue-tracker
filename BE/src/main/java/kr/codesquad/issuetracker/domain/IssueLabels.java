57f9cf82d214787222ec2c65efc1e4c0894349d7
leekm0310 <leekm310@gmail.com>
2022-06-27 21:39:37 +0900

    Refactor: @JoinColumn 추가
    - 연관관계가 있는 곳들에 @JoinColumn 추가

--- BE/src/main/java/kr/codesquad/issuetracker/domain/IssueLabels.java
+++ BE/src/main/java/kr/codesquad/issuetracker/domain/IssueLabels.java
@@ -1,18 +1,23 @@
 package kr.codesquad.issuetracker.domain;
 
+import lombok.Getter;
+
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
+@Getter
 public class IssueLabels {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = LAZY)
+    @JoinColumn
     private Issue issue;
 
     @ManyToOne(fetch = LAZY)
+    @JoinColumn
     private Label label;
 }
