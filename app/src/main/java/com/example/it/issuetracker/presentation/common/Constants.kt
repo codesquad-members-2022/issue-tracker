a0d891136995abc84c19e3bf9e39303ea2d36797
banjjak2 <interr0bang@naver.com>
2022-06-27 11:20:54 +0900

    refactor: Label 목록 관련 상태를 저장을 각각의 변수에서 UI State 로 변경
    상태 관련 변수들을 하나의 데이터클래스로 둠으로써 상태를 관리하기 편하다고 판단
      - 기존과 같이 변수로 처리했을 때는 상태의 개수가 많아질수록 변수들이 많아지게 되어 실수할 가능성이 있음

---
+++ app/src/main/java/com/example/it/issuetracker/presentation/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.it.issuetracker.presentation.common
+
+object Constants {
+    const val INIT_ERROR_MSG_ID = -1
+}
--- app/src/main/java/com/example/it/issuetracker/presentation/main/label/LabelViewModel.kt
+++ app/src/main/java/com/example/it/issuetracker/presentation/main/label/LabelViewModel.kt
@@ -4,33 +4,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.it.issuetracker.R
 import com.example.it.issuetracker.data.dto.toLabel
-import com.example.it.issuetracker.domain.model.Label
 import com.example.it.issuetracker.domain.repository.LabelRepository
 import com.example.it.issuetracker.presentation.main.issue.list.Mode
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class LabelViewModel(
     private val labelRepository: LabelRepository,
 ) : ViewModel() {
 
-    private var _labelList = MutableStateFlow<List<Label>>(emptyList())
-    val labelList = _labelList.asStateFlow()
-
-    private var _dataLoading = MutableStateFlow(false)
-    val dataLoading = _dataLoading.asStateFlow()
-
-    private var _editMode = MutableStateFlow(false)
-    val editMode = _editMode.asStateFlow()
-
-    private var _completeDelete = MutableStateFlow(false)
-    val completeDelete = _completeDelete.asStateFlow()
-
-    private var _error = MutableSharedFlow<Int>()
-    val error = _error.asSharedFlow()
+    private var _uiState = MutableStateFlow(LabelUiState())
+    val uiState = _uiState.asStateFlow()
 
     init {
         getLabelInfoList()
@@ -38,49 +24,39 @@ class LabelViewModel(
 
     fun getLabelInfoList() {
         viewModelScope.launch {
-            _dataLoading.value = true
-
+            _uiState.update { it.copy(dataLoading = true) }
             val result = labelRepository.getLabelInfoList()
             if (result.isSuccess) {
-                _labelList.value = result.getOrDefault(emptyList()).map { labelDto ->
-                    labelDto.toLabel()
-                }
+                val labelList = result.getOrDefault(emptyList()).map { it.toLabel() }
+                _uiState.update { it.copy(labelList = labelList) }
             } else {
-                _error.emit(R.string.network_error)
+                _uiState.update { it.copy(errorMsgId = R.string.network_error) }
             }
-            _dataLoading.value = false
-            _completeDelete.value = false
+            _uiState.update { it.copy(dataLoading = false, completeTask = false) }
         }
     }
 
     fun changeEditMode(editMode: Boolean) {
-        if (editMode == _editMode.value) return
-
-        val editModeList = _labelList.value.map {
-            if (it.mode == Mode.DEFAULT) {
-                it.copy(mode = Mode.EDIT)
-            } else {
-                it.copy(mode = Mode.DEFAULT)
-            }
+        if (editMode == _uiState.value.editMode) return
+        val editModeList = _uiState.value.labelList.map { label ->
+            if (label.mode == Mode.DEFAULT) label.copy(mode = Mode.EDIT)
+            else label.copy(mode = Mode.DEFAULT)
         }
-
-        _editMode.value = editMode
-        _labelList.value = editModeList
+        _uiState.update { it.copy(editMode = editMode, labelList = editModeList) }
     }
 
     fun deleteItems() {
-        _completeDelete.value = false
         viewModelScope.launch {
-            _labelList.value.filter {
-                it.isChecked
-            }.forEach {
-                val result = labelRepository.deleteLabelInfo(it.id)
+            _uiState.update { it.copy(completeTask = false) }
+            val labelList = _uiState.value.labelList
+            labelList.filter { it.isChecked }.forEach { label ->
+                val result = labelRepository.deleteLabelInfo(label.id)
                 if (result.isFailure) {
-                    _error.emit(R.string.network_error)
+                    _uiState.update { it.copy(errorMsgId = R.string.network_error) }
                     return@launch
                 }
             }
-            _completeDelete.value = true
+            _uiState.update { it.copy(completeTask = true) }
         }
     }
 }
