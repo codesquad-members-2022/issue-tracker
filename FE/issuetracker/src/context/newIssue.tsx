4d0e012b27cdb1ac05a0254da4ddc4836c90daff
SeungHyun <fm10033@gmail.com>
2022-06-28 20:57:29 +0900

    Feat : atom으로 사이드 바 상태 구현

--- FE/issuetracker/src/context/newIssue.tsx
+++ FE/issuetracker/src/context/newIssue.tsx
@@ -12,17 +12,21 @@ type CommentType = {
   description: string;
 };
 
-type newIssue = {
+type newIssueType = {
   title: string;
   writer: WriterType;
   writtenTime: string;
   comments: Array<CommentType>;
   mileStone: MileStoneType;
-  labels: LabelType;
-  assignees: AccountType;
+  labels: Array<LabelType>;
+  assignees: Array<AccountType>;
+  closed: boolean;
 };
-
-const initialState: newIssue = {
+type initialStateType = {
+  key: string;
+  default: newIssueType;
+};
+const newIssue: newIssueType = {
   title: 'FE 이슈트래커 디자인 시스템 구현',
   writer: {
     name: 'Oni',
@@ -39,44 +43,33 @@ const initialState: newIssue = {
       description:
         '처음부터 전부 구현하려고 하지 말고 필수적인 기능부터 만든 후, 차근차근 완성도를 높여보세요',
     },
-    {
-      writer: {
-        name: 'Daniel',
-        imgUrl: userImageURL2,
-      },
-      writtenTime: '2022-06-22T16:37:04',
-      description: '마감일은 언제인가요??',
-    },
   ],
-  milestone: {
-    title: '이슈트래커 1주차',
-    description: '개발 1주차',
-    dueDate: '2022-06-19',
-    progress: 0.6666666666666666,
-    openedIssue: 2,
-    closedIssue: 1,
+  mileStone: {
+    title: '',
+    description: '',
+    dueDate: '',
+    progress: 0,
+    openedIssue: 0,
+    closedIssue: 0,
   },
   labels: [
     {
       title: 'documentation',
       description: '서비스에 대한 개선 사항 혹은 추가 사항',
-      color: {
-        font: 'gray1',
-        background: 'blue2',
-      },
+      color: 'blue',
     },
   ],
   assignees: [
     {
-      id: 1,
-      name: 'Oni',
-      imageUrl: userImageURL,
-    },
-    {
-      id: 2,
-      name: 'Daniel',
-      imageUrl: userImageURL2,
+      name: 'O',
+      profileImage: '',
+      email: '',
     },
   ],
   closed: false,
 };
+const initialState: initialStateType = {
+  key: 'newIssueState',
+  default: newIssue,
+};
+export const newIssueState = atom(initialState);
