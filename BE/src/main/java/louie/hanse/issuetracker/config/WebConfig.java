944ea8023934e8874079296b336ccc5494dea0dd
Louie <dhdustnr0134@naver.com>
2022-06-28 14:57:16 +0900

    Update gradle.yml

--- BE/src/main/java/louie/hanse/issuetracker/config/WebConfig.java
+++ BE/src/main/java/louie/hanse/issuetracker/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST");
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+            .allowedHeaders("headers");
     }
 }
