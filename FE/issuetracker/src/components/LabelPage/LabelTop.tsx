f78b1e08c61bbe35b57aaeacd17dfa8c9fa081e3
soralee <sora2821@gmail.com>
2022-06-28 15:54:41 +0900

    fix: conflict 해결

--- FE/issuetracker/src/components/LabelPage/LabelTop.tsx
+++ FE/issuetracker/src/components/LabelPage/LabelTop.tsx
@@ -1,20 +1,8 @@
 import { useState } from 'react';
-<<<<<<< HEAD
 import LabelAndMileStoneBtns from 'components/common/LabelAndMileStoneBtns';
 import NewLabelList from 'components/LabelPage/NewLabelList';
 
 function LabelTop() {
-=======
-import * as I from 'design/icons';
-import * as S from 'components/LabelPage/styled/styled.labelTop';
-import NewLabelList from 'components/LabelPage/NewLabelList';
-
-interface Props {
-  $activeLabel: boolean;
-}
-
-function LabelTop({ $activeLabel }: Props) {
->>>>>>> origin/29-featfe-사이드-바-상태-구현
   const [isClicked, setIsClicked] = useState(false);
   const isNewLabel = true;
   const showLabel = () => {
@@ -22,36 +10,11 @@ function LabelTop({ $activeLabel }: Props) {
   };
   return (
     <>
-<<<<<<< HEAD
       <LabelAndMileStoneBtns
         isAddButtonClicked={isClicked}
         handleAddButtonClick={showLabel}
         handleCloseButtonClick={showLabel}
       />
-=======
-      <S.labelTop>
-        <S.tabBar>
-          <S.leftBar to="/label" $activeLabel={$activeLabel}>
-            <I.tag />
-            <S.LinkText>레이블</S.LinkText>
-            <S.numberText>(3)</S.numberText>
-          </S.leftBar>
-          <S.rightBar to="/milestone">
-            <I.milestone />
-            <S.LinkText>마일스톤</S.LinkText>
-            <S.numberText>(2)</S.numberText>
-          </S.rightBar>
-        </S.tabBar>
-        <S.addButton
-          onClick={() => {
-            showLabel();
-          }}
-        >
-          <I.plus />
-          <S.addButtonText>추가</S.addButtonText>
-        </S.addButton>
-      </S.labelTop>
->>>>>>> origin/29-featfe-사이드-바-상태-구현
       {isClicked && <NewLabelList isNewLabel={isNewLabel} />}
     </>
   );
