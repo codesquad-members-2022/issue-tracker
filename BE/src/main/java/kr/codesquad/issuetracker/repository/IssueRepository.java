26ecc18a6ec2b1d464174850edcb0319e325743f
leekm0310 <leekm310@gmail.com>
2022-06-29 17:51:42 +0900

    Feat: 이슈 상태 변경 기능 구현

--- BE/src/main/java/kr/codesquad/issuetracker/repository/IssueRepository.java
+++ BE/src/main/java/kr/codesquad/issuetracker/repository/IssueRepository.java
@@ -2,7 +2,14 @@ package kr.codesquad.issuetracker.repository;
 
 import kr.codesquad.issuetracker.domain.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IssueRepository extends JpaRepository<Issue, Long>, CustomIssueRepository {
 
+    @Modifying
+    @Query("update Issue i set i.status = :status where i.id in (:issueList)")
+    void updateStatus(boolean status, List<Long> issueList);
 }
