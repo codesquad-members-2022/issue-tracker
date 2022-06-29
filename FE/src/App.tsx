ad576b5d60a127fa4a9fb2482545acacebe84819
nanjeong <whdwkxj7@naver.com>
2022-06-29 17:35:29 +0900

    Refactor: 라우팅 부분 dynamic import, lazy loading 적용

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,13 +2,25 @@ import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '@/login/LoginPage';
 import IssueListPage from '@/issueList/IssueListPage';
-import LabelListPage from './labelList/LabelListPage';
-import MilestoneListPage from './milestoneList/MilestoneListPage';
-import AddIssuePage from './addIssue/AddIssuePage';
-import IssueDetailPage from './issueDetail/IssueDetailPage';
 import GlobalStyles from './GlobalStyles';
 import theme from './theme';
 import Layout from './common/Layout';
+import { lazy, Suspense } from 'react';
+
+const pages = ['addIssue', 'issueDetail', 'labelList', 'milestoneList'].sort();
+
+const getFileName = (page: string) => {
+  const fileName = `${page[0].toUpperCase()}${page.slice(1)}Page`;
+
+  return fileName;
+};
+
+const getPage = (page: string) => {
+  const fileName = getFileName(page);
+  const Page = lazy(() => import('./pages/' + fileName));
+
+  return <Suspense fallback={<div>Loading...</div>}>{<Page />}</Suspense>;
+};
 
 export default function App() {
   return (
@@ -19,10 +31,9 @@ export default function App() {
           <Route path="login" element={<LoginPage />} />
           <Route path="/" element={<Layout />}>
             <Route path="issueList" element={<IssueListPage />} />
-            <Route path="labelList" element={<LabelListPage />} />
-            <Route path="milestoneList" element={<MilestoneListPage />} />
-            <Route path="addIssue" element={<AddIssuePage />} />
-            <Route path="issueDetail" element={<IssueDetailPage />} />
+            {pages.map((page, idx) => {
+              return <Route key={idx} path={page} element={getPage(page)} />;
+            })}
           </Route>
         </Routes>
       </BrowserRouter>
