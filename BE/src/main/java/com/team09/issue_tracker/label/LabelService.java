0352f1361fea3d46efd670ce0243d0b22b55d619
mybloom <baejungeun11@gmail.com>
2022-06-29 15:19:32 +0900

    feat: 라벨id의 유효성 확인로직 LabelService에 작성  (bugpigg/issue-tracker#45)

--- BE/src/main/java/com/team09/issue_tracker/label/LabelService.java
+++ BE/src/main/java/com/team09/issue_tracker/label/LabelService.java
@@ -1,5 +1,6 @@
 package com.team09.issue_tracker.label;
 
+import com.team09.issue_tracker.exception.LabelInvalidException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,20 @@ public class LabelService {
 
 	private final LabelRepository labelRepository;
 
-	public boolean isMyLabels(List<Long> labelIds, Long memberId) {
-		long countOfLabelFromDb = labelRepository.countByIdInAndMemberId(labelIds, memberId);
+	public void  validateLabelIds(List<Long> labelIds, Long memberId) {
+		if (labelIds.size() > 0) {
+			validateMyLabelIds(labelIds, memberId);
+		}
+	}
 
+	private void validateMyLabelIds(List<Long> labelIds, Long memberId) {
+		if (!isMyLabels(labelIds, memberId)) {
+			throw new LabelInvalidException();
+		}
+	}
+
+	private boolean isMyLabels(List<Long> labelIds, Long memberId) {
+		long countOfLabelFromDb = labelRepository.countByIdInAndMemberId(labelIds, memberId);
 		return countOfLabelFromDb == labelIds.size();
 	}
 }
