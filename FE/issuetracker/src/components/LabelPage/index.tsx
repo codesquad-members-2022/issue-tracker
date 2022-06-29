41bf7d7411f09ffbdd8d9373aa1dad0a4ece6df5
soralee <sora2821@gmail.com>
2022-06-29 11:16:51 +0900

    refactor: labelTop confilt 해결

--- FE/issuetracker/src/components/LabelPage/index.tsx
+++ FE/issuetracker/src/components/LabelPage/index.tsx
@@ -4,11 +4,7 @@ import LabelList from 'components/LabelPage/LabelList';
 function LabelPage() {
   return (
     <>
-<<<<<<< HEAD
       <LabelTop />
-=======
-      <LabelTop $activeLabel={isLabelPage} />
->>>>>>> origin/29-featfe-사이드-바-상태-구현
       <LabelList />
     </>
   );
