6be415a28ca43ee9d1e74bb617606f582867636e
Sangbeom Heo <mansaout@gmail.com>
2022-06-29 17:51:26 +0900

    feat: 필터 옵션 목서버 get 요청추가

--- FE/src/mocks/handlers.ts
+++ FE/src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { rest } from 'msw';
+import issueFilterOptions from './issueFilterOptions';
 import issueListData from './issueListData';
 
 const handlers = [
@@ -6,6 +7,22 @@ const handlers = [
     const status = req.url.searchParams.get('status');
 
     return res(ctx.status(200), ctx.json(issueListData[status]));
+  }),
+
+  rest.get('/api/issue/labels', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(issueFilterOptions.labels));
+  }),
+
+  rest.get('/api/issue/milestones', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(issueFilterOptions.milestones));
+  }),
+
+  rest.get('/api/issue/members', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(issueFilterOptions.members));
+  }),
+
+  rest.get('/api/issue/authors', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(issueFilterOptions.members));
   })
 ];
 
