baef109b813de0a1e73f3aec35faa0826f892dfb
mybloom <baejungeun11@gmail.com>
2022-06-29 22:06:36 +0900

    feat: IssueAssignee  유효성 체크 로직을 IssueAssigneeService로 이동  (bugpigg/issue-tracker#45)

--- BE/src/main/java/com/team09/issue_tracker/issue/IssueAssigneeService.java
+++ BE/src/main/java/com/team09/issue_tracker/issue/IssueAssigneeService.java
@@ -1,8 +1,10 @@
 package com.team09.issue_tracker.issue;
 
+import com.team09.issue_tracker.exception.MemberInvalidException;
 import com.team09.issue_tracker.issue.domain.Issue;
 import com.team09.issue_tracker.issue.domain.IssueAssignee;
 import com.team09.issue_tracker.member.Member;
+import com.team09.issue_tracker.member.MemberService;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,6 +17,8 @@ public class IssueAssigneeService {
 
 	private final IssueAssigneeRepository issueAssigneeRepository;
 
+	private final MemberService memberService;
+
 	public void savedIssueAssignee(Issue issue, List<Long> assigneeIds) {
 		if (assigneeIds.size() > 0) {
 			List<IssueAssignee> issueAssignees = assigneeIds.stream()
@@ -26,6 +30,18 @@ public class IssueAssigneeService {
 
 			//연관관계 편의메서드
 			issue.addIssueAssignee(assigneeSet);
+		}
+	}
+
+	public void validateAssigneeIds(List<Long> assigneeIds) {
+		if (assigneeIds.size() > 0) {
+			validateMember(assigneeIds);
+		}
+	}
+
+	public void validateMember(List<Long> assigneeIds) {
+		if (!memberService.validateMemberIds(assigneeIds)) {
+			throw new MemberInvalidException();
 		}
 	}
 }
