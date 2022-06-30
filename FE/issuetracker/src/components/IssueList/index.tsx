2e3e13c4ccd710206588801a558de96854b4c1e2
SeungHyun <fm10033@gmail.com>
2022-06-30 21:24:07 +0900

    Feat : 이슈리스트 드롭다운 구현

--- FE/issuetracker/src/components/IssueList/index.tsx
+++ FE/issuetracker/src/components/IssueList/index.tsx
@@ -1,15 +1,48 @@
 import * as S from 'components/IssueList/styled.index';
 import * as I from 'design/icons';
+import { useState } from 'react';
 import Issues from './Issues';
 
 function IssueList() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function toggleDropdown() {
+    setIsClicked(!isClicked);
+  }
   return (
     <S.IssueListWrap>
       <S.IssueListTop>
         <S.FilterBar>
-          <S.Filter>
+          <S.Filter
+            onClick={() => {
+              toggleDropdown();
+            }}
+          >
             필터
             <I.dropdownArrow />
+            <S.Dropdown isClicked={isClicked}>
+              <S.DropdownTitle>이슈 필터</S.DropdownTitle>
+              <S.DropdownContent>
+                열린 이슈
+                <I.offCheckCircle />
+              </S.DropdownContent>
+              <S.DropdownContent>
+                내가 작성한 이슈
+                <I.offCheckCircle />
+              </S.DropdownContent>
+              <S.DropdownContent>
+                나에게 할당된 이슈
+                <I.offCheckCircle />
+              </S.DropdownContent>
+              <S.DropdownContent>
+                내가 댓글을 남긴 이슈
+                <I.offCheckCircle />
+              </S.DropdownContent>
+              <S.DropdownContent>
+                닫힌 이슈
+                <I.offCheckCircle />
+              </S.DropdownContent>
+            </S.Dropdown>
           </S.Filter>
           <S.SearchBar placeholder="is:issue is:open" />
         </S.FilterBar>
