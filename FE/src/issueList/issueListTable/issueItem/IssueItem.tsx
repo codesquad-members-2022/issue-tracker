17c7b21157135dce678c1de4949316a54b5a3cbd
nanjeong <whdwkxj7@naver.com>
2022-06-30 12:02:31 +0900

    Rename: utils/utils 파일에서 utils/time으로 변경
    - utils/index.ts 추가

--- FE/src/issueList/issueListTable/issueItem/IssueItem.tsx
+++ FE/src/issueList/issueListTable/issueItem/IssueItem.tsx
@@ -3,7 +3,7 @@ import { COLORS, GREYSCALE } from '@/common/constants';
 import { getRandomKey } from '@/utils';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { getTimeStamp } from '@/common/utils/utils';
+import { getTimeStamp } from '@/common/utils';
 import CheckBox from '@/issueList/issueListTable/CheckBox';
 import { IssueStateType } from '@/issueList/issueListTable/IssueList';
 import { useIssueListContext } from '@/issueList/stores/IssueListProvider';
