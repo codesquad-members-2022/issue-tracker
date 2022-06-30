749ed0b9f2d0bb518fc20faf1ea1d24a232b6233
seok <dhdustnr0134@naver.com>
2022-06-30 15:25:33 +0900

    refactor : cascade 옵션을 통해 최대한 적은 repository를 사용하도록 변경

--- BE/src/main/java/louie/hanse/issuetracker/service/IssueService.java
+++ BE/src/main/java/louie/hanse/issuetracker/service/IssueService.java
@@ -19,11 +19,8 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class IssueService {
     private final IssueRepository issueRepository;
-    private final CommentRepository commentRepository;
-    private final IssueManagerRepository issueManagerRepository;
     private final MemberRepository memberRepository;
     private final LabelRepository labelRepository;
-    private final IssueLabelRepository issueLabelRepository;
     private final MilestoneRepository milestoneRepository;
 
     @Transactional
@@ -31,31 +28,24 @@ public class IssueService {
         Member writer = memberRepository.findById(memberId)
             .orElseThrow(IllegalStateException::new);
         Issue issue = new Issue(issueSaveRequest.getTitle(), writer);
-        log.info("issueSaveRequest {}", issueSaveRequest);
-        issueRepository.save(issue);
         if (issueSaveRequest.getContents() != null) {
             Comment comment = new Comment(issue, issueSaveRequest.getContents());
-            commentRepository.save(comment);
-        }
-        if (!issueSaveRequest.getManagerIds().isEmpty()) {
-            List<Member> managers = memberRepository.findAllById(issueSaveRequest.getManagerIds());
-            for (Member manager : managers) {
-                IssueManager issueManager = new IssueManager(issue, manager);
-                issueManagerRepository.save(issueManager);
-            }
-        }
-        if (!issueSaveRequest.getLabelIds().isEmpty()) {
-            List<Label> labels = labelRepository.findAllById(issueSaveRequest.getLabelIds());
-            for (Label label : labels) {
-                IssueLabel issueLabel = new IssueLabel(issue, label);
-                issueLabelRepository.save(issueLabel);
-            }
         }
         if (issueSaveRequest.getMilestoneId() != null) {
             Milestone milestone = milestoneRepository.findById(issueSaveRequest.getMilestoneId())
                 .orElseThrow(IllegalStateException::new);
             issue.updateMilestone(milestone);
         }
+
+        List<Member> managers = memberRepository.findAllById(issueSaveRequest.getManagerIds());
+        for (Member manager : managers) {
+            IssueManager issueManager = new IssueManager(issue, manager);
+        }
+        List<Label> labels = labelRepository.findAllById(issueSaveRequest.getLabelIds());
+        for (Label label : labels) {
+            IssueLabel issueLabel = new IssueLabel(issue, label);
+        }
+        issueRepository.save(issue);
     }
 
     public IssueSearchResponse search(IssueSearchRequest issueSearchRequest) {
