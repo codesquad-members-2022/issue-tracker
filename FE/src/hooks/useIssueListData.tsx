bb0c1dcdd47edbfa422ffdff6ad92d7df5b0fa13
jindonyy <rsefa94@gmail.com>
2022-07-01 02:11:53 +0900

    Refactor: printError util로 이동
    jindonyy/issue-tracker/#37

--- FE/src/hooks/useIssueListData.tsx
+++ FE/src/hooks/useIssueListData.tsx
@@ -2,12 +2,8 @@ import { getIssueList } from '@/api/issueList';
 import { getIssueCount } from '@/api/issueList';
 import { APIIssueStatusType } from '@/api/type';
 import { useQuery } from 'react-query';
-import { AxiosError } from 'axios';
 import { issueListQueryKeys, issueCountQueryKeys } from '@/api/queryKeys';
-
-const printError = (error: AxiosError) => {
-  console.error(error.message);
-};
+import printError from '@/utils/printError';
 
 function useIssueListData(status?: APIIssueStatusType) {
   return useQuery(issueListQueryKeys[status ? status : 'all'], () => getIssueList(status), {
