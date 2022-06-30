3b6ad2dc5d65812077450276bc9349bf3a70529c
soralee <sora2821@gmail.com>
2022-06-30 14:48:29 +0900

    Merge branch '35-featfe-마일스톤-기능-구현' into fe-dev

--- FE/issuetracker/src/components/LabelPage/styled/styled.labelTop.ts
+++ FE/issuetracker/src/components/LabelPage/styled/styled.labelTop.ts
@@ -1,4 +1,4 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
 import { mixin } from 'design/GlobalStyles';
 import { Link } from 'react-router-dom';
 
