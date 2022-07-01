62217c7adec6be9feea880db7942ca20a64c9495
soralee <sora2821@gmail.com>
2022-07-01 11:27:18 +0900

    feat: milestone data를 API post 요청으로 업데이트하는 기능 구현

--- FE/issuetracker/src/components/milestone/MileStone.tsx
+++ FE/issuetracker/src/components/milestone/MileStone.tsx
@@ -1,33 +1,42 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import * as I from 'design/icons';
 import * as S from 'components/milestone/styled/styled.milestone';
 import { ProgressBar } from 'components/common/Common';
 import EditAndDeleteBtn from 'components/common/LabelAndMileStoneBtns/EditAndDeleteBtn';
 import NewMileStone from 'components/milestone/NewMileStone';
-import { mileStoneType, mileStoneData } from 'store/milestone';
-import { convertPercent, getDataByKey } from 'utils/util';
+import { mileStone } from 'store/milestone';
+import { convertPercent } from 'utils/util';
+import { MileStoneType } from 'data';
+import { usePostData } from 'APIs/Api';
 
 interface Props {
   idx: number;
-  mileStone: mileStoneType;
+  mileStoneData: MileStoneType;
 }
 
-function MileStone({
-  idx,
-  mileStone: { id, title, dueDate, description, progress, openedIssue, closedIssue },
-}: Props) {
+function MileStone({ idx, mileStoneData }: Props) {
+  const [mileStoneState, setMileStoneState] = useRecoilState(mileStone);
   const [isEditButtonClick, setEditButtonClick] = useState(false);
-  const [elementId, setElementId] = useState(0);
-  const percent = convertPercent(progress);
+  const percent = convertPercent(mileStoneData.progress);
+  const mutation = usePostData(
+    'https://8fe3cd27-6f2c-47dd-8182-62d896d6f37e.mock.pstmn.io/milestone/new',
+    mileStoneState,
+  );
 
-  const handleEditButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleEditButtonClick = () => {
     setEditButtonClick(true);
-    const elementIdNumber = Number(e.currentTarget.id);
-    setElementId(elementIdNumber);
   };
 
-  const selectedMileStoneData = getDataByKey(mileStoneData, 'id', elementId);
+  const handleCloseButtonClick = () => {
+    const updatedMileStoneState = {
+      ...mileStoneState,
+      closed: true,
+    };
+    setMileStoneState(updatedMileStoneState);
+    mutation.mutate(mileStoneState);
+  };
 
   return (
     <>
@@ -37,38 +46,44 @@ function MileStone({
             <S.MileStoneIconFigure>
               <I.milestone />
             </S.MileStoneIconFigure>
-            <S.Title>{title}</S.Title>
+            <S.Title>{mileStoneData.title}</S.Title>
             <S.DueDate>
               <I.calendar />
-              {dueDate}
+              {mileStoneData.dueDate}
             </S.DueDate>
           </S.MileStoneTopLeft>
           <S.MileStoneTopRight>
-            <S.CloseMileStone>
+            <S.CloseMileStone onClick={handleCloseButtonClick}>
               <I.milestone />
               닫기
             </S.CloseMileStone>
             <EditAndDeleteBtn
-              elementId={id.toString()}
+              elementId={mileStoneData.id.toString()}
               handleEditButtonClick={handleEditButtonClick}
               handleDeleteButtonClick={() => {}}
             />
           </S.MileStoneTopRight>
         </S.MileStoneTop>
         <S.MileStoneBottom>
-          <S.Discription>{description}</S.Discription>
+          <S.Discription>{mileStoneData.description}</S.Discription>
           <S.MileStoneProgressBar>
             <ProgressBar percent={percent} />
             <S.ProgressStatus>
               <S.CompleteRate>{percent}%</S.CompleteRate>
               <S.IssueStatus>
-                열린 이슈 {openedIssue} 닫힌 이슈 {closedIssue}
+                열린 이슈 {mileStoneData.openedIssue} 닫힌 이슈 {mileStoneData.closedIssue}
               </S.IssueStatus>
             </S.ProgressStatus>
           </S.MileStoneProgressBar>
         </S.MileStoneBottom>
       </S.MileStoneWrap>
-      {isEditButtonClick && <NewMileStone mileStoneData={selectedMileStoneData} />}
+      {isEditButtonClick && (
+        <NewMileStone
+          isNewMileStone={false}
+          mileStoneData={mileStoneData}
+          setEditButtonClick={setEditButtonClick}
+        />
+      )}
     </>
   );
 }
