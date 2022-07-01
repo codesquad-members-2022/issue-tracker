ef1d50f74f68d061d2987f5f5ccb31c73f5c15be
shim506 <shim506@naver.com>
2022-07-01 20:51:59 +0900

    fix: 빌드 오류 수정

--- app/src/main/java/com/example/issu_tracker/data/local/IssueTrackerDatabase.kt
+++ app/src/main/java/com/example/issu_tracker/data/local/IssueTrackerDatabase.kt
@@ -7,7 +7,7 @@ import com.example.issu_tracker.data.Converters
 import com.example.issu_tracker.data.IssueList
 import com.example.issu_tracker.data.User
 
-@Database(entities = [User::class , IssueList.Issue::class ], version = 2)
+@Database(entities = [User::class , IssueList.Issue::class ], version = 2,exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class IssueTrackerDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
