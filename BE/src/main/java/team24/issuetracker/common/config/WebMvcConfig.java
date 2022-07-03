beacc858d64f5e32eb95a18039bdd5131fddac32
PhilSoGooood <peelhw@gmail.com>
2022-07-03 15:26:18 +0900

    feat: CORS를 위한 인터셉터 추가

--- BE/src/main/java/team24/issuetracker/common/config/WebMvcConfig.java
+++ BE/src/main/java/team24/issuetracker/common/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package team24.issuetracker.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,13 @@ public class WebMvcConfig  implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("https://d1w58c8iubxw1z.cloudfront.net", "http://localhost:3000")
+			.allowedHeaders("x-requested-with, origin, content-type, accept")
+			.exposedHeaders("")
 			.allowCredentials(true);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
 	}
 }
