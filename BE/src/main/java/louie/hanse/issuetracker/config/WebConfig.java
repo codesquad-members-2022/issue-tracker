39b34bb728eced9da2ff8d5168ba104142e55f83
rkolx <olx.rko.o@gmail.com>
2022-07-06 11:35:49 +0900

    refactor: controller에서 enum을 바로 받을 수 있게 구현 및 Querydsl 상태개수 중복 수정

--- BE/src/main/java/louie/hanse/issuetracker/config/WebConfig.java
+++ BE/src/main/java/louie/hanse/issuetracker/config/WebConfig.java
@@ -1,6 +1,8 @@
 package louie.hanse.issuetracker.config;
 
+import louie.hanse.issuetracker.converter.StringToStatusConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
             .allowedHeaders("headers");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToStatusConverter());
     }
 }
--- BE/src/main/java/louie/hanse/issuetracker/domain/Status.java
+++ BE/src/main/java/louie/hanse/issuetracker/domain/Status.java
@@ -1,7 +1,15 @@
 package louie.hanse.issuetracker.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Status {
     OPEN, CLOSE;
+
+    @JsonCreator
+    public static Status valueOfWithCaseInsensitive(String name) {
+        name = name.toUpperCase();
+        return valueOf(name);
+    }
 
     public Status reverse() {
         return this == OPEN ? CLOSE : OPEN;
