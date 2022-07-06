21a14ed5240b5976b035bb6d4f1e231396c90812
Louie <dhdustnr0134@naver.com>
2022-07-06 12:07:20 +0900

    feat : 코멘트 작성 기능 구현

---
+++ BE/src/main/java/louie/hanse/issuetracker/web/controller/CommentController.java
@@ -0,0 +1,26 @@
+package louie.hanse.issuetracker.web.controller;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import louie.hanse.issuetracker.service.CommentService;
+import louie.hanse.issuetracker.web.dto.comment.CommentSaveResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/issues/{id}/comments")
+@RestController
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public CommentSaveResponse writeComment(@PathVariable Long id,
+        @RequestBody Map<String, String> map) {
+        String contents = map.get("contents");
+        return commentService.write(id, contents);
+    }
+}
